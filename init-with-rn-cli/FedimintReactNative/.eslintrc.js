71cc081b52d65508fd234ae47a3d165edf57fdaa
Oscar Lafarga <otech47@gmail.com>
2022-11-11 23:45:55 -0600

    Tweak linter to remove semicolon warnings

--- init-with-rn-cli/FedimintReactNative/.eslintrc.js
+++ init-with-rn-cli/FedimintReactNative/.eslintrc.js
@@ -3,14 +3,17 @@ module.exports = {
     extends: '@react-native-community',
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
+    rules: {
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        'semi': 'off',
+    },
     overrides: [
         {
             files: ['*.ts', '*.tsx'],
             rules: {
                 '@typescript-eslint/no-shadow': ['error'],
-                'no-shadow': 'off',
-                'no-undef': 'off',
             },
         },
     ],
-};
+}
--- init-with-rn-cli/FedimintReactNative/.prettierrc.js
+++ init-with-rn-cli/FedimintReactNative/.prettierrc.js
@@ -2,8 +2,9 @@ module.exports = {
     arrowParens: 'avoid',
     bracketSameLine: true,
     bracketSpacing: true,
+    quoteProps: 'consistent',
     singleQuote: true,
     semi: false,
     tabWidth: 4,
     trailingComma: 'all',
-};
+}
