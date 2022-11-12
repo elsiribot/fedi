9f701adede8568f49245d77944a492d1d615c92b
Oscar Lafarga <otech47@gmail.com>
2022-11-11 23:38:23 -0600

    Tweak linting settings

--- init-with-rn-cli/FedimintReactNative/.prettierrc.js
+++ init-with-rn-cli/FedimintReactNative/.prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    singleQuote: true,
+    semi: false,
+    tabWidth: 4,
+    trailingComma: 'all',
 };
