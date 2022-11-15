847abf1822154d38b4de0207b654f825b43a8016
Oscar Lafarga <otech47@gmail.com>
2022-11-15 06:48:08 -0600

    Fix for dark mode + fix TS errors in App.tsx

--- init-with-rn-cli/FedimintReactNative/App.tsx
+++ init-with-rn-cli/FedimintReactNative/App.tsx
@@ -1,9 +1,13 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React, { useEffect } from 'react'
+import {
+    NavigationContainer,
+    DarkTheme,
+    DefaultTheme,
+} from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { useTranslation } from 'react-i18next'
 import RNFS from 'react-native-fs'
-import { NativeModules } from 'react-native'
+import { NativeModules, useColorScheme } from 'react-native'
 
 import ConfirmSend from './screens/ConfirmSend'
 import Home from './screens/Home'
@@ -29,6 +33,7 @@ const Stack = createNativeStackNavigator<RootStackParamList>()
 
 const App = () => {
     const { t } = useTranslation()
+    const scheme = useColorScheme()
 
     // Initializes the connection to the federation
     async function initialize() {
@@ -39,38 +44,42 @@ const App = () => {
         initialize()
     }, [])
 
+    console.log('scheme')
+    console.log(scheme)
+
     return (
-        <NavigationContainer>
+        <NavigationContainer
+            theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
             <Stack.Navigator>
                 <Stack.Screen
                     name="Splash"
                     component={Splash}
-                    options={{ title: t('words.fedimint') }}
+                    options={{ title: `${t('words.fedimint')}` }}
                 />
                 <Stack.Screen
                     name="Home"
                     component={Home}
-                    options={{ title: t('words.home') }}
+                    options={{ title: `${t('words.home')}` }}
                 />
                 <Stack.Screen
                     name="Send"
                     component={Send}
-                    options={{ title: t('words.send') }}
+                    options={{ title: `${t('words.send')}` }}
                 />
                 <Stack.Screen
                     name="ConfirmSend"
                     component={ConfirmSend}
-                    options={{ title: t('feature.send.confirm-send') }}
+                    options={{ title: `${t('feature.send.confirm-send')}` }}
                 />
                 <Stack.Screen
                     name="Receive"
                     component={Receive}
-                    options={{ title: t('phrases.receive-bitcoin') }}
+                    options={{ title: `${t('phrases.receive-bitcoin')}` }}
                 />
                 <Stack.Screen
                     name="LnInvoice"
                     component={LnInvoice}
-                    options={{ title: t('phrases.receive-bitcoin') }}
+                    options={{ title: `${t('phrases.receive-bitcoin')}` }}
                 />
             </Stack.Navigator>
         </NavigationContainer>
