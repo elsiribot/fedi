691fb2362b7f24fd1886200667e1e2675b153f13
Oscar Lafarga <otech47@gmail.com>
2022-11-23 17:52:22 -0500

    bridge: Add target toolchain to ios

--- bridge/ios.sh
+++ bridge/ios.sh
@@ -1,5 +1,7 @@
 rustup install nightly-x86_64-apple-darwin
+rustup install nightly-aarch64-apple-darwin
 rustup component add rust-src --toolchain nightly-x86_64-apple-darwin
+rustup component add rust-src --toolchain nightly-aarch64-apple-darwin
 rustup target add aarch64-apple-ios x86_64-apple-ios
 rustup target add aarch64-apple-ios-sim --toolchain nightly
 rustup target add aarch64-apple-darwin x86_64-apple-darwin
