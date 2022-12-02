911fbd828536e76ccac3c3204656e5e46a83e879
Oscar Lafarga <otech47@gmail.com>
2022-12-01 23:37:56 -0500

    Add signet fed and persistence key to constants

--- constants.ts
+++ constants.ts
@@ -1,1 +1,6 @@
+// Regtest
 export const TEST_FEDERATION = '{"members":[[0,"ws://188.166.55.8:4001"]]}'
+// Signet
+// export const TEST_FEDERATION = '{"members":[[0,"wss://fm-signet.sirion.io:443"]]}'
+
+export const FEDERATIONS_PERSISTENCE_KEY = 'AsyncStorage-FederationsContext'
