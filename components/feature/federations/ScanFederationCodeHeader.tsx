bb9c78580fd7e0448f79d03473c7cff51496cc0b
Oscar Lafarga <otech47@gmail.com>
2022-12-03 17:26:13 -0500

    Move custom headers out of Router
    and into their respective feature component
    directories

---
+++ components/feature/federations/ScanFederationCodeHeader.tsx
@@ -0,0 +1,32 @@
+import { useNavigation } from '@react-navigation/native'
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+import { Icon, Text } from '@rneui/themed'
+import { useTranslation } from 'react-i18next'
+
+import Header from '../../ui/Header'
+
+const ScanFederationCodeHeader: React.FC<{}> = () => {
+    const { t } = useTranslation()
+    const navigation = useNavigation()
+
+    return (
+        <Header
+            headerLeft={
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <Icon name={'angle-left'} type="font-awesome" />
+                </TouchableOpacity>
+            }
+            headerCenter={
+                <Text h4>
+                    {t('feature.federations.scan-federation-invite')}
+                </Text>
+            }
+            centerContainerStyle={{
+                flex: 3,
+            }}
+        />
+    )
+}
+
+export default ScanFederationCodeHeader
