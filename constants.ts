51981183f190a6c0e245185803610401bb6cacd9
Oscar Lafarga <otech47@gmail.com>
2022-12-06 18:45:14 -0500

    Organize federation strings

--- constants.ts
+++ constants.ts
@@ -1,8 +1,12 @@
-// Regtest
-export const TEST_FEDERATION =
+// Regtest feds
+export const FEDERATION_ALPHA =
     '{"members":[[0,"wss://alpha.regtest.sirion.io"]]}'
-export const TEST_FEDERATION_ID = 'alpha'
-// Signet
-// export const TEST_FEDERATION = '{"members":[[0,"wss://fm-signet.sirion.io:443"]]}'
+export const FEDERATION_BRAVO =
+    '{"members":[[0,"wss://bravo.regtest.sirion.io"]]}'
+// Signet feds
+export const FEDERATION_SIGNET =
+    '{"members":[[0,"wss://fm-signet.sirion.io:443"]]}'
 
+export const TEST_FEDERATION = FEDERATION_ALPHA
+export const TEST_FEDERATION_ID = 'alpha'
 export const FEDERATIONS_PERSISTENCE_KEY = 'AsyncStorage-FederationsContext'
