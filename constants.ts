51c74453f8e9ed11704d563c0d890e89a370625b
Justin Moon <mail@justinmoon.com>
2022-12-06 19:11:19 -0600

    Update default federation constants

--- constants.ts
+++ constants.ts
@@ -1,6 +1,7 @@
 // Regtest
-export const TEST_FEDERATION = '{"members":[[0,"ws://188.166.55.8:4001"]]}'
-export const TEST_FEDERATION_ID = 'testfed'
+export const TEST_FEDERATION =
+    '{"members":[[0,"wss://alpha.regtest.sirion.io"]]}'
+export const TEST_FEDERATION_ID = 'alpha'
 // Signet
 // export const TEST_FEDERATION = '{"members":[[0,"wss://fm-signet.sirion.io:443"]]}'
 
