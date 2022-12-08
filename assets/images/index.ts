e96829268c1aa3cac2afab3e22592d94b6bb0a88
Oscar Lafarga <otech47@gmail.com>
2022-12-07 20:42:27 -0500

    Add icons for settings page

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,8 +1,14 @@
 export const Images = {
     AllowCameraAccessIcon: require('./allow-camera-access-icon.png'),
+    Federation: require('./federation.png'),
     FederationXIconXs: require('./federationx-icon-xs.png'),
     FederationXIconSm: require('./federationx-icon-sm.png'),
     FediLogo: require('./fedi-logo.png'),
+    FediLogoIcon: require('./fedi-logo-icon.png'),
     FediQrLogo: require('./fedi-qr-logo.png'),
     HoloBackground: require('./holo-background.jpg'),
+    InviteMembers: require('./invite-members.png'),
+    LeaveFederation: require('./leave-federation.png'),
+    Recovery: require('./recovery.png'),
+    Wallet: require('./wallet.png'),
 }
