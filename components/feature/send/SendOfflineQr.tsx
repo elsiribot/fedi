14e9eea03381dfe9df5b719c2da27e19d4f98ef6
Justin Moon <mail@justinmoon.com>
2022-12-10 19:41:14 -0600

    Hacky animiated QR display

--- components/feature/send/SendOfflineQr.tsx
+++ components/feature/send/SendOfflineQr.tsx
@@ -1,7 +1,7 @@
 import { useNavigation } from '@react-navigation/native'
 import type { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { Button } from '@rneui/themed'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Dimensions, StyleSheet, View } from 'react-native'
 import QRCode from 'react-native-qrcode-svg'
 import { Images } from '../../../assets/images'
@@ -13,13 +13,28 @@ export type Props = NativeStackScreenProps<RootStackParamList, 'SendOfflineQr'>
 const SendOfflineQr: React.FC<Props> = ({ route }: Props) => {
     const { ecash } = route.params
     const qrCodeSize = Dimensions.get('window').width * 0.8
+    const [index, setIndex] = useState(0)
     const navigation = useNavigation()
     const onDone = async () => {
         navigation.navigate('Home')
     }
+
+    // show new qr every second
+    useEffect(() => {
+        const interval = setInterval(() => {
+            console.log('setting index')
+            setIndex((index + 1) % ecash.length)
+        }, 100)
+        return () => clearInterval(interval)
+    }, [index, ecash])
+
     return (
         <View style={styles.container}>
-            <QRCode value={ecash} size={qrCodeSize} logo={Images.FediQrLogo} />
+            <QRCode
+                value={ecash[index]}
+                size={qrCodeSize}
+                logo={Images.FediQrLogo}
+            />
             <Button title={'done'} onPress={onDone} />
         </View>
     )
