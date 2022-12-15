acbfc0bf4dcc4db9fc30f099d03949af0c986bff
Oscar Lafarga <otech47@gmail.com>
2022-12-15 17:15:20 -0500

    Add HoloCard component
    for rendering commonly used holographic background
    with rounded borders that contains some content

---
+++ components/ui/HoloCard.tsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import {
+    ImageBackground,
+    ImageSourcePropType,
+    StyleSheet,
+    View,
+} from 'react-native'
+import { Image, Text, Theme, useTheme } from '@rneui/themed'
+import { Images } from '../../assets/images'
+
+type HoloCardProps = {
+    iconImage?: ImageSourcePropType | null
+    title?: string | null
+    body: React.ReactNode
+}
+
+const HoloCard: React.FC<HoloCardProps> = ({
+    iconImage = null,
+    title = null,
+    body,
+}: HoloCardProps) => {
+    const { theme } = useTheme()
+
+    return (
+        <ImageBackground
+            source={Images.HoloBackground}
+            style={styles(theme).container}
+            imageStyle={styles(theme).roundedBorder}>
+            <View style={styles(theme).innerContainer}>
+                {iconImage && (
+                    <Image source={iconImage} style={styles(theme).iconImage} />
+                )}
+                {title && (
+                    <Text h4 h4Style={styles(theme).titleText}>
+                        {title}
+                    </Text>
+                )}
+                {body}
+            </View>
+        </ImageBackground>
+    )
+}
+
+const styles = (theme: Theme) =>
+    StyleSheet.create({
+        container: {
+            width: '100%',
+            alignItems: 'center',
+            marginVertical: theme.spacing.sm,
+        },
+        iconImage: {
+            height: theme.sizes.sm,
+            width: theme.sizes.sm,
+        },
+        innerContainer: {
+            width: '100%',
+            alignItems: 'center',
+            padding: theme.spacing.xl,
+        },
+        roundedBorder: {
+            borderRadius: 12,
+        },
+        titleText: {
+            marginVertical: theme.spacing.lg,
+        },
+    })
+
+export default HoloCard
