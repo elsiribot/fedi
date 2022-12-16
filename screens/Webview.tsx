061f2acf60b319e7f93d05ebfc7bf9fda3f83335
Justin Moon <mail@justinmoon.com>
2022-12-16 18:57:36 -0500

    Implement makeInvoice WebLN handler

--- screens/Webview.tsx
+++ screens/Webview.tsx
@@ -1,16 +1,19 @@
 import type { BottomTabScreenProps } from '@react-navigation/bottom-tabs'
 import React, { MutableRefObject, useRef, useState } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { Alert, StyleSheet, View } from 'react-native'
 import { WebView } from 'react-native-webview'
 import { injectJs, onMessageHandler } from 'react-native-webln'
 
 import { RequestInvoiceArgs, KeysendArgs } from 'webln'
 import type { RootStackParamList } from '../types/navigation'
+import { useBridge } from '../contexts/FederationsContext'
+import amountUtils from '../utils/AmountUtils'
 
 export type Props = BottomTabScreenProps<RootStackParamList, 'Webview'>
 
 const Webview: React.FC<Props> = ({ route }) => {
     const { url } = route.params
+    const { generateInvoice } = useBridge()
     // FIXME: is this type casting acceptable?
     const webview = useRef<WebView>() as MutableRefObject<WebView>
     const [jsInjected, setJsInjected] = useState(false)
@@ -24,9 +27,59 @@ const Webview: React.FC<Props> = ({ route }) => {
             const node = { alias: 'fixme', pubkey: 'fixme' }
             return { node }
         },
-        makeInvoice: async (args: string | number | RequestInvoiceArgs) => {
-            console.log('makeinvoice', args)
-            return { paymentRequest: 'fixme' }
+        makeInvoice: async (data: string | number | RequestInvoiceArgs) => {
+            console.log('makeinvoice', data)
+            // FIXME: copied from blixt
+            let amount: number
+            let description = ''
+            if (typeof data === 'string') {
+                amount = Number.parseInt(data, 10)
+            } else if (typeof data === 'number') {
+                amount = data
+            } else {
+                if (typeof data.amount === 'string') {
+                    amount = Number.parseInt(data.amount, 10)
+                } else if (typeof data.amount === 'number') {
+                    amount = data.amount
+                } else {
+                    amount = 0
+                }
+                description = data.defaultMemo || ''
+            }
+
+            try {
+                await new Promise((resolve, reject) => {
+                    Alert.alert(
+                        'Invoice request',
+                        `Website wants to pay you ${amount} sats. Do you want to accept that?`,
+                        [
+                            {
+                                text: 'Yes',
+                                style: 'default',
+                                onPress: () => resolve(null),
+                            },
+                            {
+                                text: 'No',
+                                style: 'default',
+                                onPress: () => reject(),
+                            },
+                        ],
+                    )
+                })
+
+                console.log(amount, description)
+                const invoice = await generateInvoice(
+                    amountUtils.satToMsat(amount),
+                    description,
+                )
+
+                return {
+                    paymentRequest: invoice,
+                }
+            } catch (e) {
+                console.error('Error creating invoice', e)
+                throw new Error('Denied')
+            }
         },
         sendPayment: async (paymentRequest: string) => {
             console.log('sendPayment', paymentRequest)
