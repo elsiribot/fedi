a69599b3d9db0a06f4287befe4034c24c6f07d0a
Justin Moon <mail@justinmoon.com>
2022-12-29 15:28:42 -0600

    bridge: Revert "Switch fedimint client dependency"
    This reverts commit 978ab85eaebe4bbfb08240e96269d04101f3782a.

--- bridge/calculator-ffi/src/bridge.rs
+++ bridge/calculator-ffi/src/bridge.rs
@@ -57,7 +57,7 @@ async fn get_federations(
                 let cfg: UserClientConfig = load_from_file(&path).expect("invalid cfg on disk"); // FIXME: this panics
                 path.set_extension("db");
                 let db = SledDb::open(path, "client").unwrap(); // FIXME: don't unwrap
-                let client = UserClient::new(cfg.clone(), db.into(), Default::default());
+                let client = UserClient::new(cfg.clone(), db.into(), Default::default()).await;
                 let federation = Arc::new(Federation::new(client, event_sink.clone()));
                 federations.insert(cfg.0.federation_name, federation);
             }
@@ -198,7 +198,8 @@ impl Federation {
         // Create user client
         let db_path = Path::new(&data_dir).join(format!("{}.db", cfg.federation_name));
         let db = SledDb::open(db_path, "client")?;
-        let client = UserClient::new(UserClientConfig(cfg.clone()), db.into(), Default::default());
+        let client =
+            UserClient::new(UserClientConfig(cfg.clone()), db.into(), Default::default()).await;
         Ok(Self::new(client, event_sink))
     }
 
@@ -439,6 +440,7 @@ impl Federation {
         // TODO: need to expose the database
         let mut dbtx = self.dbtx();
         dbtx.insert_entry(&TransactionKey(tx.id.clone()), tx)
+            .await
             .expect("Db error");
         dbtx.commit_tx().await.expect("Db error");
         // notify UI
@@ -481,6 +483,7 @@ impl Federation {
         tracing::info!("saving payment");
         let mut dbtx = self.dbtx();
         dbtx.insert_entry(&PaymentKey(payment.invoice.payment_hash().clone()), payment)
+            .await
             .expect("Db error");
         dbtx.commit_tx().await.expect("Db error");
         tracing::info!("saved payment");
@@ -504,6 +507,7 @@ impl Federation {
             payment.status = status;
             let mut dbtx = self.dbtx();
             dbtx.insert_entry(&PaymentKey(*payment_hash), &payment)
+                .await
                 .expect("Db error");
             dbtx.commit_tx().await.expect("Db error");
         }
