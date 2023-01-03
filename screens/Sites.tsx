29ba3d29fa276d2db3663dc40be2c6d4de31b5b0
Oscar Lafarga <otech47@gmail.com>
2023-01-03 02:59:45 +0100

    Add todo comment for offline state

--- screens/Sites.tsx
+++ screens/Sites.tsx
@@ -21,6 +21,8 @@ const Sites: React.FC<Props> = ({ navigation }) => {
     const renderSite: ListRenderItem<Site> = ({ item }) => {
         return <SiteTile site={item} selectSite={onSelect} />
     }
+    
+    // TODO: Add offline state as part of #53
     return (
         <View style={styles(theme).container}>
             <Text h2 medium h2Style={styles(theme).title}>
