8bd2808454bb6c2d2a2b41a240f30bd89c8399b7
Justin Moon <mail@justinmoon.com>
2023-01-20 23:32:09 -0600

    bridge: Disable tracing-oslog
    Wouldn't compile

--- bridge/calculator-ffi/src/logging.rs
+++ bridge/calculator-ffi/src/logging.rs
@@ -73,24 +73,24 @@ impl<'a> tracing::field::Visit for StringVisitor<'a> {
 
 // TODO: configurable log level
 pub fn init_logging(event_sink: Arc<EventSinkWrapper>) {
-    #[cfg(target_os = "android")]
+    // #[cfg(target_os = "android")]
     tracing_subscriber::registry()
         .with(ReactNativeLayer(event_sink).with_filter(LevelFilter::INFO))
         .try_init()
         .unwrap_or_else(|error| tracing::info!("Error installing logger: {}", error));
-    #[cfg(target_os = "ios")]
-    use tracing_subscriber::{layer::SubscriberExt, prelude::*, Layer};
-    #[cfg(target_os = "ios")]
-    tracing_subscriber::registry()
-        .with(
-            tracing_oslog::OsLogger::new(
-                "com.justinmoon.fluttermint",
-                "INFO", // I don't know what this does ...
-            )
-            .with_filter(tracing_subscriber::filter::LevelFilter::INFO),
-        )
-        .try_init()
-        .unwrap_or_else(|error| tracing::info!("Error installing logger: {}", error));
+    // #[cfg(target_os = "ios")]
+    // use tracing_subscriber::{layer::SubscriberExt, prelude::*, Layer};
+    // #[cfg(target_os = "ios")]
+    // tracing_subscriber::registry()
+    //     .with(
+    //         tracing_oslog::OsLogger::new(
+    //             "com.justinmoon.fluttermint",
+    //             "INFO", // I don't know what this does ...
+    //         )
+    //         .with_filter(tracing_subscriber::filter::LevelFilter::INFO),
+    //     )
+    //     .try_init()
+    //     .unwrap_or_else(|error| tracing::info!("Error installing logger: {}", error));
     #[cfg(target_os = "macos")]
     tracing_subscriber::fmt()
         .try_init()
