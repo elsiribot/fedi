956503fa86f6ca3fedf67d250481d209477c20d2
Justin Moon <mail@justinmoon.com>
2023-01-23 10:52:27 -0600

    bridge: Uncomment things from ios.sh

--- bridge/ios.sh
+++ bridge/ios.sh
@@ -1,13 +1,13 @@
 # exit on failure
 set -e 
 
-# rustup install nightly-x86_64-apple-darwin
-# rustup install nightly-aarch64-apple-darwin
-# rustup component add rust-src --toolchain nightly-x86_64-apple-darwin
-# rustup component add rust-src --toolchain nightly-aarch64-apple-darwin
-# rustup target add aarch64-apple-ios x86_64-apple-ios
-# rustup target add aarch64-apple-ios-sim --toolchain nightly
-# rustup target add aarch64-apple-darwin x86_64-apple-darwin
+rustup install nightly-x86_64-apple-darwin
+rustup install nightly-aarch64-apple-darwin
+rustup component add rust-src --toolchain nightly-x86_64-apple-darwin
+rustup component add rust-src --toolchain nightly-aarch64-apple-darwin
+rustup target add aarch64-apple-ios x86_64-apple-ios
+rustup target add aarch64-apple-ios-sim --toolchain nightly
+rustup target add aarch64-apple-darwin x86_64-apple-darwin
 
 pushd calculator-ffi
 cargo run --package ffi-bindgen -- --language swift --out-dir ../calculator-swift/Sources/Calculator
@@ -16,12 +16,12 @@ popd
 # ignoring MacOS for now
 # cargo build --package calculator-ffi --profile release-smaller --target x86_64-apple-darwin
 # cargo build --package calculator-ffi --profile release-smaller --target aarch64-apple-darwin
-# cargo build --package calculator-ffi --profile release-smaller --target x86_64-apple-ios
+cargo build --package calculator-ffi --profile release-smaller --target x86_64-apple-ios
 cargo build --package calculator-ffi --profile release-smaller --target aarch64-apple-ios
-# cargo +nightly build --package calculator-ffi --release -Z build-std --target aarch64-apple-ios-sim
+cargo +nightly build --package calculator-ffi --release -Z build-std --target aarch64-apple-ios-sim
 
-# mkdir -p target/lipo-ios-sim/release-smaller
-# lipo target/aarch64-apple-ios-sim/release/libcalculatorffi.a target/x86_64-apple-ios/release-smaller/libcalculatorffi.a -create -output target/lipo-ios-sim/release-smaller/libcalculatorffi.a
+mkdir -p target/lipo-ios-sim/release-smaller
+lipo target/aarch64-apple-ios-sim/release/libcalculatorffi.a target/x86_64-apple-ios/release-smaller/libcalculatorffi.a -create -output target/lipo-ios-sim/release-smaller/libcalculatorffi.a
 # mkdir -p target/lipo-macos/release-smaller
 # lipo target/aarch64-apple-darwin/release-smaller/libcalculatorffi.a target/x86_64-apple-darwin/release-smaller/libcalculatorffi.a -create -output target/lipo-macos/release-smaller/libcalculatorffi.a
 
