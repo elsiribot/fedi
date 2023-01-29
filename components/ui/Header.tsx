60eafa5fa8fad74b6b1ef5f5cb34d418aa00b071
Justin Moon <mail@justinmoon.com>
2023-01-29 15:09:34 -0600

    Close button in <Header/> resets navigation instead of replacing

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -2,6 +2,7 @@ import { useNavigation } from '@react-navigation/native'
 import { Header as HeaderRNE, Icon, useTheme } from '@rneui/themed'
 import React from 'react'
 import { Pressable, ViewStyle } from 'react-native'
+import { reset } from '../../state/navigation'
 
 import { NavigationHook } from '../../types/navigation'
 
@@ -78,7 +79,7 @@ const Header: React.FC<HeaderProps> = ({
     if (closeButton) {
         rightComponent = (
             <Pressable
-                onPress={() => navigation.replace('Home')}
+                onPress={() => navigation.dispatch(reset('Home'))}
                 style={{
                     padding: theme.spacing.xs,
                 }}>
