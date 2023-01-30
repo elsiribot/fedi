eefbb10738469c6882f7d6cbc6da480bbf68db06
Oscar Lafarga <otech47@gmail.com>
2023-01-30 17:38:20 -0500

    Add size prop to SvgImage

--- components/ui/SvgImage.tsx
+++ components/ui/SvgImage.tsx
@@ -4,21 +4,28 @@ import { StyleSheet, View, ViewStyle } from 'react-native'
 import { SvgProps } from 'react-native-svg'
 import * as Svgs from '../../assets/images/svgs'
 
+export enum SvgImageSize {
+    sm = 'sm',
+    md = 'md',
+    lg = 'lg',
+}
 type SvgImageProps = {
     name: string
+    size?: SvgImageSize
     containerStyle?: ViewStyle
     svgProps?: SvgProps
 }
 
-const SvgImage = ({ name, containerStyle, svgProps }: SvgImageProps) => {
+const SvgImage = ({ name, size, containerStyle, svgProps }: SvgImageProps) => {
     const { theme } = useTheme()
     const svgName = `${name}Svg`
     const Svg = Object(Svgs)[svgName]
 
+    const svgSize = size || SvgImageSize.sm
+
     const defaultSvgProps = {
-        stroke: theme.colors.primary,
-        height: theme.sizes.sm,
-        width: theme.sizes.sm,
+        height: theme.sizes[svgSize],
+        width: theme.sizes[svgSize],
     }
     const mergedSvgProps = {
         ...defaultSvgProps,
