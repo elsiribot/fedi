db7a96d94e2492288bb3f37d8a912d358e35ebf0
Oscar Lafarga <otech47@gmail.com>
2023-01-30 17:19:49 -0500

    Improve SvgImage component
    - add type safety
    - add more flexibility

--- components/ui/SvgImage.tsx
+++ components/ui/SvgImage.tsx
@@ -1,14 +1,41 @@
+import { useTheme } from '@rneui/themed'
 import React from 'react'
-import { View } from 'react-native'
+import { StyleSheet, View, ViewStyle } from 'react-native'
+import { SvgProps } from 'react-native-svg'
 import * as Svgs from '../../assets/images/svgs'
 
-const SvgImage = ({ name, style, svgStyle }) => {
+type SvgImageProps = {
+    name: string
+    containerStyle?: ViewStyle
+    svgProps?: SvgProps
+}
+
+const SvgImage = ({ name, containerStyle, svgProps }: SvgImageProps) => {
+    const { theme } = useTheme()
     const svgName = `${name}Svg`
     const Svg = Object(Svgs)[svgName]
-    console.log(React.createElement(Svg, { ...svgStyle }))
+
+    const defaultSvgProps = {
+        stroke: theme.colors.primary,
+        height: theme.sizes.sm,
+        width: theme.sizes.sm,
+    }
+    const mergedSvgProps = {
+        ...defaultSvgProps,
+        ...svgProps,
+    }
+
+    const mergedStyles = [styles.container, containerStyle]
+
     return (
-        <View style={style}>{React.createElement(Svg, { ...svgStyle })}</View>
+        <View style={mergedStyles}>
+            {React.createElement(Svg, { ...mergedSvgProps })}
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {},
+})
 
 export default SvgImage
