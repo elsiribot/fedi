29bacc9f7d2787b5940897b02c19b8da86852d2c
Oscar Lafarga <otech47@gmail.com>
2023-01-31 22:51:07 -0500

    Clean up loading states ScanFederationCode
    and prep for allowing multiple federations

--- screens/ScanFederationCode.tsx
+++ screens/ScanFederationCode.tsx
@@ -26,40 +26,53 @@ const ScanFederationCode: React.FC<Props> = ({ navigation }: Props) => {
     const { t } = useTranslation()
     const { state, dispatch } = useFederationsContext()
     const { toast } = useEnvironmentContext().state
-    const [joiningFederation, setJoiningFederation] = useState<string>('')
+    const [federationToJoin, setFederationToJoin] = useState<string>('')
+    const [joiningComplete, setJoiningComplete] = useState<boolean>(false)
 
     useEffect(() => {
         const handleJoinFederation = async () => {
             try {
-                const federation = await joinFederation(joiningFederation)
+                const federation = await joinFederation(federationToJoin)
                 const federations = await listFederations()
                 if (federations.length > 0) {
                     dispatch(updateFederations(federation.name, federations))
+                    setJoiningComplete(true)
                 }
             } catch (e) {
                 console.error(e)
                 toast?.show(t('errors.failed-to-join-federation'), 5000)
             }
-            setJoiningFederation('')
+            setFederationToJoin('')
         }
 
-        if (joiningFederation) {
+        if (federationToJoin) {
             handleJoinFederation()
         }
-    }, [dispatch, joiningFederation, t, toast])
+    }, [dispatch, federationToJoin, t, toast])
 
     useEffect(() => {
-        if (joiningFederation === '' && state.selectedFederation) {
+        if (
+            federationToJoin === '' &&
+            state.selectedFederation &&
+            // This is critical in case you are scanning to join multiple
+            // federations so selectedFederation may still be non-null
+            joiningComplete
+        ) {
             navigation.replace('FederationWelcome')
         }
-    }, [navigation, state.selectedFederation, joiningFederation])
+    }, [
+        navigation,
+        state.selectedFederation,
+        federationToJoin,
+        joiningComplete,
+    ])
 
     const handleUserInput = useCallback(
         async (input: string) => {
             if (input.startsWith('{"members":')) {
                 console.info('fedi qr code detected', input)
                 // Set the federation string to trigger the useEffect above
-                setJoiningFederation(input)
+                setFederationToJoin(input)
             } else {
                 toast?.show(t('errors.invalid-federation-code'), 5000)
             }
@@ -76,8 +89,10 @@ const ScanFederationCode: React.FC<Props> = ({ navigation }: Props) => {
     const device = devices.back
 
     const renderQrCodeScanner = () => {
-        if (device == null || joiningFederation !== '') {
+        if (device == null) {
             return <ActivityIndicator />
+        } else if (federationToJoin !== '') {
+            return null
         } else {
             return (
                 <QrCodeScanner
@@ -96,8 +111,8 @@ const ScanFederationCode: React.FC<Props> = ({ navigation }: Props) => {
                         'feature.federations.paste-federation-code-instead',
                     )}
                     onPress={checkClipboard}
-                    disabled={joiningFederation !== ''}
-                    loading={joiningFederation !== ''}
+                    disabled={federationToJoin !== ''}
+                    loading={federationToJoin !== ''}
                     type="clear"
                 />
             }
@@ -108,8 +123,8 @@ const ScanFederationCode: React.FC<Props> = ({ navigation }: Props) => {
                 </View>
                 <View style={styles(theme).buttonContainer}>
                     <Button
-                        disabled={joiningFederation !== ''}
-                        loading={joiningFederation !== ''}
+                        disabled={federationToJoin !== ''}
+                        loading={federationToJoin !== ''}
                         title={t('feature.federations.paste-federation-code')}
                         onPress={checkClipboard}
                         fullWidth
