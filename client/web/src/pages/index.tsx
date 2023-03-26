1badd0df3838496266d7ed1e41068554ab68f23a
Will O'Beirne <wbobeirne@gmail.com>
2023-03-26 12:46:31 -0500

    Example of web using common

--- client/web/src/pages/index.tsx
+++ client/web/src/pages/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import dateUtils from '@fedi/common/utils/DateUtils'
 
 function HomePage() {
-    return <div>Fedi web</div>
+    return (
+        <div>
+            Fedi web, the time is {dateUtils.formatTimestamp(Date.now() / 1000)}
+        </div>
+    )
 }
 
 export default HomePage
