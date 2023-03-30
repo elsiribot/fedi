044c208ba3044119144e7fb9eacd4b5d37d971d7
Will O'Beirne <wbobeirne@gmail.com>
2023-03-30 15:15:04 -0400

    Add eslint rule to not use rneui Icon

--- ui/native/.eslintrc.js
+++ ui/native/.eslintrc.js
@@ -3,4 +3,18 @@ module.exports = {
     extends: ['@react-native-community', 'prettier', '../.eslintrc.js'],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
+    rules: {
+        'no-restricted-imports': [
+            'error',
+            {
+                paths: [
+                    {
+                        name: '@rneui/themed',
+                        importNames: ['Icon'],
+                        message: 'Use <SvgImage /> for icons instead',
+                    },
+                ],
+            },
+        ],
+    },
 }
