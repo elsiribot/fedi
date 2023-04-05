814202c6a2999f6e881c15ba7266c4ecf1e2bcc2
Will O'Beirne <wbobeirne@gmail.com>
2023-04-05 13:37:43 -0500

    Add Template to all pages, includes navigation and federation selector

---
+++ ui/web/src/components/Template/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { styled, theme } from '../../styles'
+import { FederationSelector } from './FederationSelector'
+import { Navigation } from './Navigation'
+
+interface Props {
+    children: React.ReactNode
+}
+
+export const Template: React.FC<Props> = ({ children }) => {
+    return (
+        <Container>
+            <Navigation />
+            <Content>
+                <FederationSelector />
+                {children}
+            </Content>
+        </Container>
+    )
+}
+
+const Container = styled('div', {
+    display: 'flex',
+    minHeight: '-webkit-fill-available',
+
+    '@md': {
+        height: '-webkit-fill-available',
+        maxHeight: '100vh',
+        flexDirection: 'column-reverse',
+    },
+})
+
+const Content = styled('main', {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 48,
+    padding: 48,
+    overflow: 'auto',
+    holoGradient: '100',
+
+    '@md': {
+        padding: 36,
+        gap: 36,
+    },
+
+    '@sm': {
+        background: 'none',
+    },
+})
--- ui/web/src/pages/_app.tsx
+++ ui/web/src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { globalStyles } from '../styles'
 import type { AppProps } from 'next/app'
+import { Template } from '../components/Template'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
     globalStyles()
-    return <Component {...pageProps} />
+    return (
+        <Template>
+            <Component {...pageProps} />
+        </Template>
+    )
 }
 
 export default MyApp
