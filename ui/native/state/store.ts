c9f4cab25440108ae34da1a32d0aa726a90d5099
Will O'Beirne <wbobeirne@gmail.com>
2023-04-11 16:42:32 -0500

    Remove redux federation bridge code

--- ui/native/state/store.ts
+++ ui/native/state/store.ts
@@ -1,9 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import { commonReducers, initializeCommonStore } from '@fedi/common/redux'
-import { updateFederation } from '@fedi/common/redux/federation'
-
-import { BridgeEventEmitter } from '../bridge'
 
 export const store = configureStore({
     reducer: {
@@ -17,13 +14,4 @@ export type AppDispatch = typeof store.dispatch
 export function initializeNativeStore() {
     // Common initialization behavior
     initializeCommonStore(store.dispatch)
-
-    // Update federations on bridge event
-    const emitter = new BridgeEventEmitter()
-    emitter.onFederationUpdate(event => {
-        // Prevents a state update on the off-chance we get an event
-        // before the selectedFederation state is initialized
-        if (store.getState().federation.activeFederationId == null) return
-        store.dispatch(updateFederation(event))
-    })
 }
