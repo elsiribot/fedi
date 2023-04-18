fe61fda3061c5692701b70892945d98ff8819b31
Will O'Beirne <wbobeirne@gmail.com>
2023-04-18 14:40:02 -0500

    feature(web): Use showToastError and formatErrorMessage for all user facing errors

--- ui/web/src/hooks/toast.ts
+++ ui/web/src/hooks/toast.ts
@@ -1,21 +1,32 @@
 import { useCallback } from 'react'
+import { useTranslation } from 'react-i18next'
 
 import {
     showToast as reduxShowToast,
     closeToast as reduxCloseToast,
 } from '@fedi/common/redux'
 import { ToastArgs } from '@fedi/common/types'
+import { formatErrorMessage } from '@fedi/common/utils/format'
 
 import { useAppDispatch } from './store'
 
 export function useToast() {
+    const { t } = useTranslation()
     const dispatch = useAppDispatch()
 
     const showToast = useCallback(
-        (toast: ToastArgs) => {
-            dispatch(reduxShowToast(toast))
+        (toast: string | ToastArgs) => {
+            const args = typeof toast === 'string' ? { content: toast } : toast
+            dispatch(reduxShowToast(args))
         },
         [dispatch],
+    )
+
+    const showErrorToast = useCallback(
+        (err: unknown, defaultMsg: string) => {
+            showToast({ content: formatErrorMessage(t, err, defaultMsg) })
+        },
+        [t, showToast],
     )
 
     const closeToast = useCallback(
@@ -25,5 +36,5 @@ export function useToast() {
         [dispatch],
     )
 
-    return { showToast, closeToast }
+    return { showToast, showErrorToast, closeToast }
 }
