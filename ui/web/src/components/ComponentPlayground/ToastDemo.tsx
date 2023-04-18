9e11d1893aa9f85d61647a69dcb0889a764adb8f
Will O'Beirne <wbobeirne@gmail.com>
2023-04-18 13:54:19 -0500

    feature(web): Add playground entry for toasts

---
+++ ui/web/src/components/ComponentPlayground/ToastDemo.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+import { Button } from '../../components/Button'
+import { useToast } from '../../hooks'
+import { styled } from '../../styles'
+import { Input } from '../Input'
+
+export const ToastDemo: React.FC = () => {
+    const [content, setContent] = useState(
+        'Failed to toast, requires at least one toaster to be available.',
+    )
+    const { showToast, closeToast } = useToast()
+
+    return (
+        <Container>
+            <Input
+                label="content"
+                value={content}
+                onChange={ev => setContent(ev.currentTarget.value)}
+            />
+            <Button variant="primary" onClick={() => showToast({ content })}>
+                Open toast
+            </Button>
+        </Container>
+    )
+}
+
+const Container = styled('div', {
+    display: 'flex',
+    maxWidth: 320,
+    flexDirection: 'column',
+    gap: 16,
+})
