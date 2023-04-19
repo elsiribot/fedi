519f73d0a0f1b8bbb298513805475c8bf64a8509
Will O'Beirne <wbobeirne@gmail.com>
2023-04-19 16:31:00 -0500

    feature(web): add QRCode component

---
+++ ui/web/src/components/QRCode.tsx
@@ -0,0 +1,35 @@
+import { create as createQrCode } from 'qrcode'
+import React, { useEffect, useState } from 'react'
+
+import { styled, theme } from '../styles'
+import { renderStyledQrSvg } from '../utils/qrcode'
+
+interface Props {
+    data: string
+}
+
+export const QRCode: React.FC<Props> = ({ data }) => {
+    const [qrSvg, setQrSvg] = useState('')
+
+    useEffect(() => {
+        setQrSvg(renderStyledQrSvg(createQrCode(data)))
+    }, [data])
+
+    return (
+        <Container>
+            <Inner dangerouslySetInnerHTML={{ __html: qrSvg }} />
+        </Container>
+    )
+}
+
+const Container = styled('div', {
+    holoGradient: '900',
+    padding: 4,
+    borderRadius: 20,
+})
+
+const Inner = styled('div', {
+    background: theme.colors.white,
+    padding: 20,
+    borderRadius: 16,
+})
