8587787c48e12ef8709b3425292d4c9ef3685554
Will O'Beirne <wbobeirne@gmail.com>
2023-04-24 12:32:28 -0500

    feature(web): implement federation selector popover

--- ui/web/src/components/Template/FederationSelector.tsx
+++ ui/web/src/components/Template/FederationSelector.tsx
@@ -1,39 +1,95 @@
+import Link from 'next/link'
 import React, { useCallback, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 
-import ChevronRight from '@fedi/common/assets/svgs/chevron-right.svg'
-import { selectActiveFederation } from '@fedi/common/redux'
+import ChevronRightIcon from '@fedi/common/assets/svgs/chevron-right.svg'
+import PlusIcon from '@fedi/common/assets/svgs/plus.svg'
+import {
+    selectActiveFederation,
+    selectFederations,
+    setActiveFederationId,
+} from '@fedi/common/redux'
+import { Federation } from '@fedi/common/types'
+import amountUtils from '@fedi/common/utils/AmountUtils'
 
-import { useAppSelector } from '../../hooks'
-import { styled } from '../../styles'
+import { useAppDispatch, useAppSelector } from '../../hooks'
+import { styled, theme } from '../../styles'
 import { Avatar } from '../Avatar'
 import { Icon } from '../Icon'
+import { Popover } from '../Popover'
 import { Text } from '../Text'
 
 export const FederationSelector: React.FC = () => {
+    const { t } = useTranslation()
+    const dispatch = useAppDispatch()
     const activeFederation = useAppSelector(selectActiveFederation)
+    const federations = useAppSelector(selectFederations)
     const [isSelectorOpen, setIsSelectorOpen] = useState(false)
 
-    const toggleSelectorOpen = useCallback(() => {
-        setIsSelectorOpen(isOpen => !isOpen)
-    }, [])
+    const handleSelectFederation = useCallback(
+        (fed: Federation) => {
+            dispatch(setActiveFederationId(fed.id))
+            setIsSelectorOpen(false)
+        },
+        [dispatch],
+    )
 
     if (!activeFederation) return null
 
+    const federationList = (
+        <FederationList>
+            {federations.map(fed => (
+                <li key={fed.id}>
+                    <FederationItem
+                        active={fed.id === activeFederation.id}
+                        onClick={() => handleSelectFederation(fed)}>
+                        <Avatar size="sm" shape="hexagon" name={fed.name} />
+                        <div>
+                            <Text variant="caption" weight="bold">
+                                {fed.name}
+                            </Text>
+                            <Text variant="small">
+                                {amountUtils.formatSats(
+                                    amountUtils.msatToSat(fed.balance),
+                                )}{' '}
+                                {t('words.sats')}
+                            </Text>
+                        </div>
+                    </FederationItem>
+                </li>
+            ))}
+            <li>
+                <FederationItem add as={Link} href="/onboarding/join">
+                    <Icon icon={PlusIcon} size="sm" />
+                    <Text variant="caption" weight="bold">
+                        {t('feature.federations.add-federation')}
+                    </Text>
+                </FederationItem>
+            </li>
+        </FederationList>
+    )
+
     return (
         <Container>
-            <ActiveFederation onClick={toggleSelectorOpen}>
-                <Avatar
-                    size="sm"
-                    shape="hexagon"
-                    name={activeFederation.name}
-                />
-                <Text variant="caption" weight="bold">
-                    {activeFederation.name}
-                </Text>
-                <IconWrapper isOpen={isSelectorOpen}>
-                    <Icon size="xs" icon={ChevronRight} />
-                </IconWrapper>
-            </ActiveFederation>
+            <Popover
+                content={federationList}
+                sideOffset={10}
+                open={isSelectorOpen}
+                onOpenChange={setIsSelectorOpen}>
+                <ActiveFederation>
+                    <Avatar
+                        size="sm"
+                        shape="hexagon"
+                        name={activeFederation.name}
+                    />
+                    <Text variant="caption" weight="bold">
+                        {activeFederation.name}
+                    </Text>
+                    <IconWrapper isOpen={isSelectorOpen}>
+                        <Icon size="xs" icon={ChevronRightIcon} />
+                    </IconWrapper>
+                </ActiveFederation>
+            </Popover>
         </Container>
     )
 }
@@ -57,6 +113,47 @@ const IconWrapper = styled('div', {
         isOpen: {
             true: {
                 transform: 'rotate(90deg)',
+            },
+        },
+    },
+})
+
+const FederationList = styled('ul', {
+    width: 260,
+    padding: 0,
+    margin: -8,
+
+    '& > li': {
+        listStyle: 'none',
+    },
+})
+
+const FederationItem = styled('button', {
+    display: 'flex',
+    alignItems: 'center',
+    textAlign: 'left',
+    width: '100%',
+    gap: 8,
+    padding: 8,
+    borderRadius: 8,
+    backgroundColor: `transparent`,
+    transition: 'background-color 100ms ease',
+
+    '&:hover': {
+        backgroundColor: theme.colors.primary10,
+    },
+
+    variants: {
+        active: {
+            true: {},
+        },
+        add: {
+            true: {
+                opacity: 0.6,
+
+                '&:hover': {
+                    opacity: 1,
+                },
             },
         },
     },
