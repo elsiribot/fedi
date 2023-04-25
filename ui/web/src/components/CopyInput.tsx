55c074c7a8b3ae07a4fe86ff6f989bc703235014
Will O'Beirne <wbobeirne@gmail.com>
2023-04-25 15:46:21 -0500

    feature(web): add CopyInput component using new Input adornment prop

---
+++ ui/web/src/components/CopyInput.tsx
@@ -0,0 +1,56 @@
+import React, { useCallback } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import CopyIcon from '@fedi/common/assets/svgs/copy.svg'
+
+import { useToast } from '../hooks'
+import { styled } from '../styles'
+import { Button } from './Button'
+import { Icon } from './Icon'
+import { Input } from './Input'
+import { Text } from './Text'
+
+interface Props {
+    value: string
+    label?: React.ReactNode
+    onCopyMessage?: string
+}
+
+export const CopyInput: React.FC<Props> = ({ value, label, onCopyMessage }) => {
+    const { t } = useTranslation()
+    const { showToast, showErrorToast } = useToast()
+
+    const handleCopy = useCallback(async () => {
+        try {
+            navigator.clipboard.writeText(value)
+            if (onCopyMessage) {
+                showToast({ content: t(onCopyMessage as any) })
+            }
+        } catch (err) {
+            showErrorToast(err, 'errors.unknown-error')
+        }
+    }, [value, showToast, onCopyMessage, t, showErrorToast])
+
+    return (
+        <Input
+            value={value}
+            label={label}
+            adornment={
+                <CopyButton onClick={handleCopy}>
+                    <Icon icon={CopyIcon} />
+                    <Text variant="small" weight="medium">
+                        {t('words.copy')}
+                    </Text>
+                </CopyButton>
+            }
+            readOnly
+        />
+    )
+}
+
+const CopyButton = styled('button', {
+    display: 'flex',
+    alignItems: 'center',
+    paddingRight: 12,
+    gap: 4,
+})
