cc99651b34e771293a534600f68d2b2c8e864069
Will O'Beirne <wbobeirne@gmail.com>
2023-04-27 14:12:54 -0500

    feature(web): add multi prop to QRScanner to handle scanning qrloop codes

--- ui/web/src/components/QRScanner.tsx
+++ ui/web/src/components/QRScanner.tsx
@@ -1,38 +1,77 @@
 import type QrScanner from 'qr-scanner'
+import {
+    areFramesComplete,
+    framesToData,
+    parseFramesReducer,
+    progressOfFrames,
+    State as FrameState,
+} from 'qrloop'
 import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { theme } from '@fedi/common/constants/theme'
 import { useUpdatingRef } from '@fedi/common/hooks/util'
 import { formatErrorMessage } from '@fedi/common/utils/format'
 
-import { styled } from '../styles'
+import { styled, theme } from '../styles'
 import { Text } from './Text'
 
 export type ScanResult = QrScanner.ScanResult
 
 interface Props {
+    multi?: boolean
     onScan(result: ScanResult): void
 }
 
-export const QRScanner: React.FC<Props> = ({ onScan }) => {
+export const QRScanner: React.FC<Props> = ({ multi, onScan }) => {
     const { t } = useTranslation()
     const [videoEl, setVideoEl] = useState<HTMLVideoElement | null>(null)
     const qrScannerRef = useRef<QrScanner | null>(null)
     const [mediaError, setMediaError] = useState<string>()
+    const [_frames, setFrames] = useState<FrameState | null>(null)
+    const [progress, setProgress] = useState(0)
 
+    // Maintain a ref to onScan to avoid re-running useEffects
     const onScanRef = useUpdatingRef(onScan)
 
+    // Accumulate frames
+    const handleMultiScan = useCallback(
+        (result: ScanResult) => {
+            console.log(result.data)
+            setFrames(oldFrames => {
+                try {
+                    const newFrames = parseFramesReducer(oldFrames, result.data)
+                    if (areFramesComplete(newFrames)) {
+                        onScanRef.current({
+                            data: framesToData(newFrames).toString(),
+                            cornerPoints: result.cornerPoints,
+                        })
+                    }
+                    setProgress(progressOfFrames(newFrames))
+                    return newFrames
+                } catch (err) {
+                    setMediaError(
+                        formatErrorMessage(t, err, 'errors.unknown-error'),
+                    )
+                    return null
+                }
+            })
+        },
+        [t, onScanRef],
+    )
+
+    // Maintain a ref that switches between handlers based on multi
+    const handleScanRef = useUpdatingRef(multi ? handleMultiScan : onScan)
+
+    // Sets up QrScanner using device camera
     const handleScannerSetup = useCallback(async () => {
         if (!videoEl) return
         try {
             const QrScanner = (await import('qr-scanner')).default
             const qrScanner = new QrScanner(
                 videoEl,
-                result => onScanRef.current(result),
+                result => handleScanRef.current(result),
                 {
                     returnDetailedScanResult: true,
-                    maxScansPerSecond: 5,
                     onDecodeError: () => null, // no-op
                 },
             )
@@ -41,14 +80,15 @@ export const QRScanner: React.FC<Props> = ({ onScan }) => {
         } catch (err) {
             setMediaError(formatErrorMessage(t, err, 'errors.unknown-error'))
         }
-    }, [videoEl, onScanRef, t])
+    }, [videoEl, handleScanRef, t])
 
+    // Setup scanner on mount, tear down on unmount
     useEffect(() => {
         handleScannerSetup()
         return () => {
             const qrScanner = qrScannerRef.current
             if (qrScanner) {
-                qrScanner.stop()
+                qrScanner.destroy()
                 qrScannerRef.current = null
             }
         }
@@ -57,6 +97,14 @@ export const QRScanner: React.FC<Props> = ({ onScan }) => {
     return (
         <Container>
             <Video ref={setVideoEl} />
+            {multi && !!progress && (
+                <Progress>
+                    <ProgressBar style={{ width: `${progress * 100}%` }} />
+                    <ProgressPercent>
+                        {Math.round(progress * 100)}%
+                    </ProgressPercent>
+                </Progress>
+            )}
             {mediaError && (
                 <Error>
                     <Text variant="small">{mediaError}</Text>
@@ -81,6 +129,36 @@ const Video = styled('video', {
     borderRadius: 16,
     background: theme.colors.extraLightGrey,
     objectFit: 'cover',
+})
+
+const Progress = styled('div', {
+    position: 'absolute',
+    left: 16,
+    right: 16,
+    bottom: 16,
+    height: 12,
+    borderRadius: 6,
+    background: theme.colors.primary20,
+    overflow: 'hidden',
+})
+
+const ProgressBar = styled('div', {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    background: theme.colors.primary,
+    borderRadius: 6,
+})
+
+const ProgressPercent = styled('div', {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    fontSize: theme.fontSizes.tiny,
+    color: theme.colors.white,
+    textShadow: `0 1px 1px ${theme.colors.primary}`,
 })
 
 const Error = styled('div', {
