786119a11add39de6aa4fc18a0cc3749440a4ca5
Oscar Lafarga <otech47@gmail.com>
2023-05-03 23:28:46 +0200

    tweak: bold federation name in header

--- ui/native/components/feature/federations/SelectedFederationHeader.tsx
+++ ui/native/components/feature/federations/SelectedFederationHeader.tsx
@@ -46,7 +46,7 @@ const SelectedFederationHeader: React.FC<{}> = () => {
                             width: 20,
                         }}
                     />
-                    <Text medium small style={styles(theme).federationName}>
+                    <Text bold small style={styles(theme).federationName}>
                         {activeFederation?.name}
                     </Text>
                     <SvgImage name="ChevronRight" size={SvgImageSize.xs} />
