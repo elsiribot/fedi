56a86f6084bfa2f24aae127a252fef85face05f9
Will O'Beirne <wbobeirne@gmail.com>
2023-05-08 15:56:20 -0500

    feature: add just build-wasm command

---
+++ scripts/build-wasm.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+cd bridge/fedi-wasm || exit
+nix develop --ignore-environment .#cross --command ./wasm-build.sh
+cd ../..
+# cp out/* ../../ui/common/wasm# 
+
+WASM_OUT="bridge/fedi-wasm/out"
+
+# fedi-wasm-ui
+mkdir -p fedi-wasm-ui/public
+mkdir -p fedi-wasm-ui/src/wasm
+cp "$WASM_OUT/fedi_wasm_bg.wasm" fedi-wasm-ui/public/fedi.wasm
+cp $WASM_OUT/*.{ts,js} fedi-wasm-ui/src/wasm
+
+# ui/common
+cp $WASM_OUT/*.{ts,js,wasm} ui/common/wasm
