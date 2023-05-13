451c6f30517f52c35c7979788868d68950af8267
Oscar Lafarga <otech47@gmail.com>
2023-05-13 03:23:39 +0200

    style: fix lint

--- ui/native/bridge.ts
+++ ui/native/bridge.ts
@@ -9,7 +9,7 @@ async function fedimintRpc<Type = void>(
     method: string,
     payload: object,
 ): Promise<Type> {
-    console.log("rpc method", method);
+    console.log('rpc method', method)
     const jsonPayload = JSON.stringify(payload)
     const json: string = await new Promise(resolve => {
         setTimeout(() => resolve(FedimintFfi.rpc(method, jsonPayload)))
