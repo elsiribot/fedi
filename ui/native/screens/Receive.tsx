d83e77ab53923d7ca036774deb896fbf4f44542d
Oscar Lafarga <otech47@gmail.com>
2023-05-14 21:09:31 +0200

    fix: error message on max invoice amount

--- ui/native/screens/Receive.tsx
+++ ui/native/screens/Receive.tsx
@@ -68,7 +68,12 @@ const Receive: React.FC<Props> = ({ navigation }: Props) => {
 
     const onChangeAmount = (updatedValue: Sats) => {
         if (maxReceiveAmount && updatedValue > maxReceiveAmount) {
-            toast?.show(t('feature.receive.maximum-invoice-amount'), 3000)
+            toast?.show(
+                t('feature.receive.maximum-invoice-amount', {
+                    maxAmount: amountUtils.formatSats(maxReceiveAmount as Sats),
+                }),
+                3000,
+            )
         } else {
             toast?.close(0)
         }
