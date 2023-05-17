6267531278093556fdb748d30ce0e1967860b71e
Will O'Beirne <wbobeirne@gmail.com>
2023-05-17 15:41:25 -0500

    refactor(ui): move sites images into common

--- ui/web/src/components/SiteTiles.tsx
+++ ui/web/src/components/SiteTiles.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 
-import { SITES } from '@fedi/common/constants/sites'
+import { selectFederationSites } from '@fedi/common/redux'
 
+import { useAppSelector } from '../hooks'
 import { styled, theme } from '../styles'
 import { Text } from './Text'
 
 export const SiteTiles: React.FC = () => {
+    const sites = useAppSelector(selectFederationSites)
+
     return (
         <Container>
-            {SITES.map(site => (
+            {sites.map(site => (
                 <SiteTile href={site.url} key={site.id}>
                     <SiteIcon />
                     <Text variant="small">{site.title}</Text>
