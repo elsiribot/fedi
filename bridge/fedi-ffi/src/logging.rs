57595d42891beaf5cb7ecd01fe11e517a4c84bdd
maan2003 <manmeetmann2003@gmail.com>
2023-05-26 02:13:37 +0530

    feat: better log events

--- bridge/fedi-ffi/src/logging.rs
+++ bridge/fedi-ffi/src/logging.rs
@@ -11,6 +11,7 @@ use std::{
 
 use anyhow::Context;
 use tracing::metadata::LevelFilter;
+use tracing_serde::AsSerde;
 use tracing_subscriber::{layer::SubscriberExt, prelude::*, EnvFilter, Layer};
 
 use crate::event::{Event, EventSink, TypedEventExt};
@@ -26,56 +27,9 @@ where
         event: &tracing::Event<'_>,
         _ctx: tracing_subscriber::layer::Context<'_, S>,
     ) {
-        let mut fields = BTreeMap::new();
-        let mut visitor = StringVisitor(&mut fields);
-        event.record(&mut visitor);
-        // TODO: implement proper formatting
-        if let Some(message) = fields.get("message") {
-            let log = format!(
-                "(rust) {}: {}",
-                event.metadata().level().to_string().to_uppercase(),
-                message
-            );
-            let event = Event::log(log);
-            self.0.typed_event(&event);
+        if let Ok(event) = serde_json::to_string(&event.as_serde()) {
+            self.0.typed_event(&Event::log(event));
         }
-    }
-}
-
-struct StringVisitor<'a>(&'a mut BTreeMap<String, String>);
-
-impl<'a> tracing::field::Visit for StringVisitor<'a> {
-    fn record_f64(&mut self, field: &tracing::field::Field, value: f64) {
-        self.0.insert(field.name().to_string(), value.to_string());
-    }
-
-    fn record_i64(&mut self, field: &tracing::field::Field, value: i64) {
-        self.0.insert(field.name().to_string(), value.to_string());
-    }
-
-    fn record_u64(&mut self, field: &tracing::field::Field, value: u64) {
-        self.0.insert(field.name().to_string(), value.to_string());
-    }
-
-    fn record_bool(&mut self, field: &tracing::field::Field, value: bool) {
-        self.0.insert(field.name().to_string(), value.to_string());
-    }
-
-    fn record_str(&mut self, field: &tracing::field::Field, value: &str) {
-        self.0.insert(field.name().to_string(), value.to_string());
-    }
-
-    fn record_error(
-        &mut self,
-        field: &tracing::field::Field,
-        value: &(dyn std::error::Error + 'static),
-    ) {
-        self.0.insert(field.name().to_string(), value.to_string());
-    }
-
-    fn record_debug(&mut self, field: &tracing::field::Field, value: &dyn std::fmt::Debug) {
-        self.0
-            .insert(field.name().to_string(), format!("{:?}", value));
     }
 }
 
