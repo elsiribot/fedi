0f2b483eb910c32f047975b90f839a0d1e25877b
Oscar Lafarga <otech47@gmail.com>
2023-05-31 00:10:00 +0200

    style: add warning exception
    for properly used nested component definitions

--- ui/native/.eslintrc.js
+++ ui/native/.eslintrc.js
@@ -3,6 +3,11 @@ module.exports = {
     extends: ['@react-native-community', 'prettier', '../.eslintrc.js'],
     parser: '@typescript-eslint/parser',
     rules: {
+        // The react-navigation library used in native takes nested components
+        // defined as props but none of those instances use the props as
+        // rendered elements which would impact virtual DOM performance so this
+        // option to remove these warnings should be safe
+        'react/no-unstable-nested-components': ['warn', { allowAsProps: true }],
         'no-restricted-imports': [
             'error',
             {
