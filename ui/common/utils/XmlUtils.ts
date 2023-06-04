565f450ce9f5d4b5801eceedd1de9b6b98fc982c
Oscar Lafarga <otech47@gmail.com>
2023-06-04 16:58:15 +0200

    fix: bad merge

--- ui/common/utils/XmlUtils.ts
+++ ui/common/utils/XmlUtils.ts
@@ -529,6 +529,36 @@ export class PublishPublicKeyQuery extends XmppQuery {
         return xml(this.tag, attributes, pubsubXml)
     }
 }
+export class PublishNotificationTokenQuery extends XmppQuery {
+    // This publishes the user's Firebase Cloud Messagin token to the Prosody
+    // server so that the mod_cloud_notify module can send push notifications
+    // when the user is not connected to the websocket
+    // https://xmpp.org/extensions/xep-0357.html
+    static id = 'publishNotificationToken'
+    args: PublishNotificationTokenArgs
+    constructor(args: PublishNotificationTokenArgs) {
+        super()
+        this.args = args
+    }
+    build = (): Element => {
+        const { token, from } = this.args
+        const fromJid: JID = jid(from!)
+
+        const attributes = {
+            id: `${PublishNotificationTokenQuery.id}-${uuidv4()}`,
+            from,
+            type: 'set',
+        }
+
+        const enableXml = xml('enable', {
+            xmlns: 'urn:xmpp:push:0',
+            jid: `push.${fromJid.domain}`,
+            node: token,
+        })
+
+        return xml(this.tag, attributes, enableXml)
+    }
+}
 export class SetMemberRoleQuery extends XmppQuery {
     static id = 'setMemberRole'
     args: SetMemberRoleArgs
@@ -557,39 +587,6 @@ export class SetMemberRoleQuery extends XmppQuery {
                 xml('item', { role, nick: username }),
             ),
         )
-    }
-}
-export class PublishNotificationTokenQuery extends XmppQuery {
-    // This publishes the user's Firebase Cloud Messagin token to the Prosody
-    // server so that the mod_cloud_notify module can send push notifications
-    // when the user is not connected to the websocket
-    // https://xmpp.org/extensions/xep-0357.html
-    static id = 'publishNotificationToken'
-    args: PublishNotificationTokenArgs
-    constructor(args: PublishNotificationTokenArgs) {
-        super()
-        this.args = args
-    }
-    build = (): Element => {
-        const { token, from } = this.args
-        const fromJid: JID = jid(from!)
-
-        const attributes = {
-            id: `${PublishNotificationTokenQuery.id}-${uuidv4()}`,
-            from,
-            type: 'set',
-        }
-
-        const enableXml = xml(
-            'enable',
-            {
-                xmlns: 'urn:xmpp:push:0',
-                jid: `push.${fromJid.domain}`,
-                node: token,
-            },
-        )
-
-        return xml(this.tag, attributes, enableXml)
     }
 }
 export class SetPubsubNodeConfigQuery extends XmppQuery {
