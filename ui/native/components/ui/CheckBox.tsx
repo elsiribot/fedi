8e0a2f47e28e57bb9adb237035b37412a27d2fbf
Will O'Beirne <wbobeirne@gmail.com>
2023-06-14 13:31:00 -0500

    refactor(native): simplify CheckBox props

--- ui/native/components/ui/CheckBox.tsx
+++ ui/native/components/ui/CheckBox.tsx
@@ -6,27 +6,18 @@ import React from 'react'
 
 import SvgImage from './SvgImage'
 
-type CheckBoxProps = CheckBoxRNEProps
+type CheckBoxProps = Omit<CheckBoxRNEProps, 'children'>
 
 const CheckBox: React.FC<CheckBoxProps> = ({
-    checked,
     checkedIcon = <SvgImage name="CheckboxChecked" />,
     uncheckedIcon = <SvgImage name="CheckboxUnchecked" />,
-    title,
-    // TODO: apply these colors to the SvgImage.color if defined
-    checkedColor,
-    uncheckedColor,
-    onPress,
+    ...props
 }: CheckBoxProps) => {
     return (
         <CheckBoxRNE
-            checked={checked}
             checkedIcon={checkedIcon}
             uncheckedIcon={uncheckedIcon}
-            {...(title && { title })}
-            {...(checkedColor && { checkedColor })}
-            {...(uncheckedColor && { uncheckedColor })}
-            {...(onPress && { onPress })}
+            {...props}
         />
     )
 }
