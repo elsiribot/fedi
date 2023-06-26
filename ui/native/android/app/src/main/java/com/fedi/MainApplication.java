e73b6fb5196056b005dcd7a9ca6be03086e18085
Will O'Beirne <wbobeirne@gmail.com>
2023-06-26 11:03:08 -0500

    fix: attempt to update all react-native packages for 0.72 compatibility, still some issues with gradle and kotlin version differences

--- ui/native/android/app/src/main/java/com/fedi/MainApplication.java
+++ ui/native/android/app/src/main/java/com/fedi/MainApplication.java
@@ -5,15 +5,15 @@ import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
-import com.fedi.newarchitecture.MainApplicationReactNativeHost;
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final DefaultReactNativeHost mReactNativeHost =
-      new ReactNativeHost(this) {
+  private final ReactNativeHost mReactNativeHost =
+      new DefaultReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
