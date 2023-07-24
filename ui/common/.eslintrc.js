38b2e40c3a94ba03632631b2c6a9260ef20001f7
Will O'Beirne <wbobeirne@gmail.com>
2023-07-24 12:11:55 -0500

    chore: configure eslint plugin:react-hooks/recommended in web and common

--- ui/common/.eslintrc.js
+++ ui/common/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
         'prettier',
         '../.eslintrc.js',
     ],
--- ui/web/.eslintrc.js
+++ ui/web/.eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'next/core-web-vitals',
+        'plugin:react-hooks/recommended',
         'prettier',
         '../.eslintrc.js',
     ],
