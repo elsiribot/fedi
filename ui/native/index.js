09239abaf0f1e16c230bd188dbd4ea046c737c59
Oscar Lafarga <otech47@gmail.com>
2023-07-26 22:44:10 -0400

    feat: add url polyfill so URL() works in native

--- ui/native/index.js
+++ ui/native/index.js
@@ -4,6 +4,7 @@
 import { AppRegistry } from 'react-native'
 import 'react-native-gesture-handler'
 import 'react-native-reanimated'
+import 'react-native-url-polyfill/auto'
 
 import App from './App'
 import { name as appName } from './app.json'
