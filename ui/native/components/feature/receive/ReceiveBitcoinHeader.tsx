c39085c6770b684c43c8d63402c4af98d2e30f63
Oscar Lafarga <otech47@gmail.com>
2023-07-28 22:58:37 -0400

    ui(native): add scan to receive
    fixes #1372

--- ui/native/components/feature/receive/ReceiveBitcoinHeader.tsx
+++ ui/native/components/feature/receive/ReceiveBitcoinHeader.tsx
@@ -1,11 +1,17 @@
-import { Text } from '@rneui/themed'
+import { useNavigation } from '@react-navigation/native'
+import { Text, Theme, useTheme } from '@rneui/themed'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { Pressable, StyleSheet } from 'react-native'
 
+import { NavigationHook } from '../../../types/navigation'
 import Header from '../../ui/Header'
+import SvgImage from '../../ui/SvgImage'
 
 const ReceiveBitcoinHeader: React.FC<{}> = () => {
     const { t } = useTranslation()
+    const { theme } = useTheme()
+    const navigation = useNavigation<NavigationHook>()
 
     return (
         <Header
@@ -13,8 +19,24 @@ const ReceiveBitcoinHeader: React.FC<{}> = () => {
             headerCenter={
                 <Text bold>{t('feature.receive.request-bitcoin')}</Text>
             }
+            headerRight={
+                <Pressable
+                    onPress={() => navigation.navigate('ReceiveOffline')}
+                    hitSlop={5}>
+                    <SvgImage name="Scan" color={theme.colors.primary} />
+                </Pressable>
+            }
+            rightContainerStyle={styles(theme).rightContainer}
         />
     )
 }
+const styles = (_theme: Theme) =>
+    StyleSheet.create({
+        rightContainer: {
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+        },
+    })
 
 export default ReceiveBitcoinHeader
