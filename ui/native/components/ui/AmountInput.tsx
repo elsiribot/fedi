5e1e1ab33d4a812cac37764db788a649670ef559
Will O'Beirne <wbobeirne@gmail.com>
2023-08-03 14:44:52 -0500

    feature: give AmountInput the validation / error suggestion treatment

--- ui/native/components/ui/AmountInput.tsx
+++ ui/native/components/ui/AmountInput.tsx
@@ -1,10 +1,11 @@
 import { Text, Theme, useTheme } from '@rneui/themed'
 import React, { RefObject, useEffect, useRef } from 'react'
-import { useTranslation } from 'react-i18next'
-import { Keyboard, Pressable, StyleSheet, TextInput } from 'react-native'
+import { Trans, useTranslation } from 'react-i18next'
+import { Keyboard, Pressable, StyleSheet, TextInput, View } from 'react-native'
 
 import { useAmountInput } from '@fedi/common/hooks/amount'
 import { Sats } from '@fedi/common/types'
+import amountUtils from '@fedi/common/utils/AmountUtils'
 
 import InvisibleInput from './InvisibleInput'
 import SvgImage from './SvgImage'
@@ -13,6 +14,8 @@ export type Props = {
     amount: Sats
     minimumAmount?: Sats | null
     maximumAmount?: Sats | null
+    submitAttempts?: number
+    verb?: string
     onChangeAmount?: (amount: Sats) => void
 }
 
@@ -20,6 +23,8 @@ const AmountInput: React.FC<Props> = ({
     amount,
     minimumAmount,
     maximumAmount,
+    submitAttempts,
+    verb,
     onChangeAmount,
 }) => {
     const { t } = useTranslation()
@@ -32,7 +37,7 @@ const AmountInput: React.FC<Props> = ({
         handleChangeFiat,
         handleChangeSats,
         currency,
-        currencySymbol,
+        validation,
     } = useAmountInput(amount, onChangeAmount, minimumAmount, maximumAmount)
     const inputRef = useRef<TextInput>(null)
 
@@ -50,39 +55,81 @@ const AmountInput: React.FC<Props> = ({
         }
     }, [])
 
-    return (
-        <Pressable
-            style={styles(theme).container}
-            onPress={() => {
-                console.debug('inputRef?.current?.focus()')
-                inputRef?.current?.focus()
-            }}>
-            {isFiat ? (
-                <InvisibleInput
-                    inputRef={inputRef as RefObject<TextInput>}
-                    onChangeText={handleChangeFiat}
-                    value={fiatValue}
-                    label={currency}
-                />
-            ) : (
-                <InvisibleInput
-                    inputRef={inputRef as RefObject<TextInput>}
-                    onChangeText={handleChangeSats}
-                    value={satsValue}
-                    label={t('words.sats').toUpperCase()}
-                />
-            )}
-            <Pressable style={styles(theme).symbolSwitcher}>
-                <Text style={styles(theme).secondaryAmountText}>
-                    {isFiat
-                        ? `${satsValue} ${t('words.sats').toUpperCase()}`
-                        : `${currencySymbol} ${fiatValue}`}
+    // Check validation for errors to render with suggestion for amount.
+    let error: React.ReactNode | undefined
+    if (validation && (!validation.onlyShowOnSubmit || submitAttempts)) {
+        const handlePressSuggestion = () => {
+            handleChangeSats(validation.amount.toString())
+        }
+        // TODO: Make only underlined suggestion pressable, <Trans /> doesn't like <Pressable /> as a component
+        // TODO: Make this wiggle when submitAttempts is incremented
+        error = (
+            <Pressable onPress={handlePressSuggestion}>
+                <Text style={styles(theme).error} caption>
+                    <Trans
+                        i18nKey={validation.i18nKey}
+                        values={{
+                            verb:
+                                verb?.toLowerCase() ||
+                                t('words.send').toLowerCase(),
+                            amount: `${amountUtils.formatSats(
+                                validation.amount,
+                            )} ${t('words.sats')}`,
+                        }}
+                        components={{
+                            suggestion: (
+                                <Text
+                                    style={styles(theme).errorSuggestion}
+                                    caption
+                                />
+                            ),
+                        }}
+                    />
                 </Text>
-                <Pressable onPress={() => setIsFiat(!isFiat)}>
-                    <SvgImage name="Switch" color={theme.colors.grey} />
+            </Pressable>
+        )
+    }
+
+    return (
+        <View style={styles(theme).container}>
+            <Pressable
+                style={styles(theme).inputs}
+                onPress={() => {
+                    console.debug('inputRef?.current?.focus()')
+                    inputRef?.current?.focus()
+                }}>
+                {isFiat ? (
+                    <InvisibleInput
+                        inputRef={inputRef as RefObject<TextInput>}
+                        onChangeText={handleChangeFiat}
+                        value={fiatValue}
+                        label={currency}
+                    />
+                ) : (
+                    <InvisibleInput
+                        inputRef={inputRef as RefObject<TextInput>}
+                        onChangeText={handleChangeSats}
+                        value={satsValue}
+                        label={t('words.sats').toUpperCase()}
+                    />
+                )}
+                <Pressable
+                    style={styles(theme).symbolSwitcher}
+                    onPress={() => setIsFiat(!isFiat)}>
+                    <Text style={styles(theme).secondaryAmountText} medium>
+                        {isFiat
+                            ? `${satsValue} ${t('words.sats').toUpperCase()}`
+                            : `${fiatValue} ${currency}`}
+                    </Text>
+                    <SvgImage
+                        name="Switch"
+                        color={theme.colors.grey}
+                        size={20}
+                    />
                 </Pressable>
             </Pressable>
-        </Pressable>
+            {error}
+        </View>
     )
 }
 
@@ -94,16 +141,30 @@ const styles = (theme: Theme) =>
             marginHorizontal: theme.spacing.xl,
             // This creates a small buffer zone to block Keyboard.dismiss
             // events from parent components
-            padding: theme.spacing.xl,
+            paddingVertical: theme.spacing.xl,
+            gap: theme.spacing.xl,
+        },
+        inputs: {
+            alignItems: 'center',
+            gap: theme.spacing.md,
         },
         secondaryAmountText: {
             color: theme.colors.darkGrey,
             textAlign: 'center',
-            marginHorizontal: theme.spacing.sm,
+            marginRight: theme.spacing.xs,
         },
         symbolSwitcher: {
             flexDirection: 'row',
             alignItems: 'center',
+        },
+        error: {
+            width: '100%',
+            color: theme.colors.red,
+            textAlign: 'center',
+        },
+        errorSuggestion: {
+            color: theme.colors.red,
+            textDecorationLine: 'underline',
         },
     })
 
