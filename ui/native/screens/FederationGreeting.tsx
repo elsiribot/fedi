97ce054277c41a5dea452e44c917fb2aeef8f4a4
Oscar Lafarga <otech47@gmail.com>
2023-08-16 15:58:55 -0400

    fix: surround unwrap with try catch

--- ui/native/screens/FederationGreeting.tsx
+++ ui/native/screens/FederationGreeting.tsx
@@ -1,6 +1,6 @@
 import type { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { Button, Text, Theme, useTheme } from '@rneui/themed'
-import React from 'react'
+import React, { useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
 import { StyleSheet, View } from 'react-native'
 
@@ -22,6 +22,21 @@ const FederationGreeting: React.FC<Props> = ({ navigation }: Props) => {
     const authenticatedMember = useAppSelector(selectAuthenticatedMember)
     const dispatch = useAppDispatch()
     const { activeFederationId } = useAppSelector(s => s.federation)
+
+    const handleContinue = useCallback(async () => {
+        try {
+            if (!activeFederationId) return
+            await dispatch(
+                connectChat({
+                    fedimint,
+                    federationId: activeFederationId,
+                }),
+            ).unwrap()
+            navigation.replace('TabsNavigator')
+        } catch (error) {
+            console.error(error)
+        }
+    }, [activeFederationId, dispatch, navigation])
 
     return (
         <View style={styles(theme).container}>
@@ -45,16 +60,7 @@ const FederationGreeting: React.FC<Props> = ({ navigation }: Props) => {
             <Button
                 fullWidth
                 title={t('feature.onboarding.continue-to-fedi')}
-                onPress={async () => {
-                    if (!activeFederationId) return
-                    await dispatch(
-                        connectChat({
-                            fedimint,
-                            federationId: activeFederationId,
-                        }),
-                    ).unwrap()
-                    navigation.replace('TabsNavigator')
-                }}
+                onPress={handleContinue}
                 containerStyle={styles(theme).button}
             />
         </View>
