1b7547e74f2797b4a12444a17c63881797dec68e
oz <otech47@gmail.com>
2023-08-21 20:58:06 -0400

    fix: use pushd / popd
    after testing that it works in CI

--- scripts/ui/bump-version-native.sh
+++ scripts/ui/bump-version-native.sh
@@ -5,7 +5,7 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 
 $REPO_ROOT/scripts/enforce-nix.sh
 
-cd $REPO_ROOT/ui
+pushd $REPO_ROOT/ui/native
 git config --global user.email "dev@fedibtc.com"
 git config --global user.name "Fedi Dev CI"
 RELEASE_BRANCH_VERSION="${GITHUB_REF##*/}"
@@ -29,6 +29,7 @@ echo "Pushing version commit to git branch"
 # git add package.json android/app/build.gradle && git commit -m "${NEW_VERSION}" && git push
 echo "Saving new version + APK path as outputs for next steps in job"
 echo "NEW_VERSION=$(npm pkg get version --ws false | sed 's/"//g')" >> $GITHUB_OUTPUT
-echo "APK_PATH=android/app/build/outputs/apk/production/release/app-production-release-${NEW_VERSION}.apk" >> $GITHUB_OUTPUT
+echo "APK_PATH=$REPO_ROOT/ui/native/android/app/build/outputs/apk/production/release/app-production-release-${NEW_VERSION}.apk" >> $GITHUB_OUTPUT
 echo "Saving latest commit to output for 'call-deployment-workflow' job"
 echo "COMMIT_TO_DEPLOY=$(git rev-parse HEAD)" >> $GITHUB_OUTPUT
+popd
