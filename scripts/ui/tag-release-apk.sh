0b84fa222937f2349123f9a48bbf74b9ee28f78a
oz <otech47@gmail.com>
2023-08-21 22:58:54 -0400

    style: fix lint

--- scripts/ui/tag-release-apk.sh
+++ scripts/ui/tag-release-apk.sh
@@ -6,9 +6,11 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 $REPO_ROOT/scripts/enforce-nix.sh
 
 pushd $REPO_ROOT/ui/native/android
+
 RELEASE_PATH=$REPO_ROOT/ui/native/android/app/build/outputs/apk/production/release
 SOURCE=$RELEASE_PATH/app-production-release.apk
 DESTINATION=./$APK_PATH
 echo "Moving apk from $SOURCE to $DESTINATION"
 mv $SOURCE $DESTINATION
+
 popd
