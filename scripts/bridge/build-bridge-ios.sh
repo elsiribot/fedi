b7400f1b8960c9ad57d47138e1bf57fe55412175
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-08-24 14:04:00 -0700

    chore(ci): tweak `nix develop`-based build caching

--- scripts/bridge/build-bridge-ios.sh
+++ scripts/bridge/build-bridge-ios.sh
@@ -10,23 +10,23 @@ cd $BRIDGE_ROOT
 TARGET=$REPO_ROOT/target
 
 cd $BRIDGE_ROOT/fedi-ffi
-nix develop .#xcode --command cargo run --package ffi-bindgen -- --language swift --out-dir $BRIDGE_ROOT/fedi-swift/Sources/Fedi
+cargo run --package ffi-bindgen -- --language swift --out-dir $BRIDGE_ROOT/fedi-swift/Sources/Fedi
 cd $BRIDGE_ROOT
 
 # use the xcode shell to make sure we have the necessary SDKs
-nix develop .#xcode --command cargo build --package fedi-ffi --profile release --target x86_64-apple-ios $CARGO_FLAGS
-nix develop .#xcode --command cargo build --package fedi-ffi --profile release --target aarch64-apple-ios $CARGO_FLAGS
-nix develop .#xcode --command cargo build --package fedi-ffi --release --target aarch64-apple-ios-sim $CARGO_FLAGS
+cargo build --package fedi-ffi ${CARGO_PROFILE:+--profile ${CARGO_PROFILE}} --target x86_64-apple-ios $CARGO_FLAGS
+cargo build --package fedi-ffi ${CARGO_PROFILE:+--profile ${CARGO_PROFILE}} --target aarch64-apple-ios $CARGO_FLAGS
+cargo build --package fedi-ffi ${CARGO_PROFILE:+--profile ${CARGO_PROFILE}} --target aarch64-apple-ios-sim $CARGO_FLAGS
 
-mkdir -p $TARGET/lipo-ios-sim/release
-nix develop .#xcode --command lipo $TARGET/aarch64-apple-ios-sim/release/libfediffi.a ../target/x86_64-apple-ios/release/libfediffi.a -create -output $TARGET/lipo-ios-sim/release/libfediffi.a
+mkdir -p $TARGET/lipo-ios-sim/${CARGO_PROFILE:-debug}
+lipo $TARGET/aarch64-apple-ios-sim/${CARGO_PROFILE:-debug}/libfediffi.a ../target/x86_64-apple-ios/${CARGO_PROFILE:-debug}/libfediffi.a -create -output $TARGET/lipo-ios-sim/${CARGO_PROFILE:-debug}/libfediffi.a
 
 cd $BRIDGE_ROOT/fedi-swift
 mv Sources/Fedi/fedi.swift Sources/Fedi/Fedi.swift || true
 cp Sources/Fedi/fediFFI.h fediFFI.xcframework/ios-arm64/fediFFI.framework/Headers
 cp Sources/Fedi/fediFFI.h fediFFI.xcframework/ios-arm64_x86_64-simulator/fediFFI.framework/Headers
 cp Sources/Fedi/fediFFI.h fediFFI.xcframework/macos-arm64_x86_64/fediFFI.framework/Headers
-cp $TARGET/aarch64-apple-ios/release/libfediffi.a fediFFI.xcframework/ios-arm64/fediFFI.framework/fediFFI
-cp $TARGET/lipo-ios-sim/release/libfediffi.a fediFFI.xcframework/ios-arm64_x86_64-simulator/fediFFI.framework/fediFFI
+cp $TARGET/aarch64-apple-ios/${CARGO_PROFILE:-debug}/libfediffi.a fediFFI.xcframework/ios-arm64/fediFFI.framework/fediFFI
+cp $TARGET/lipo-ios-sim/${CARGO_PROFILE:-debug}/libfediffi.a fediFFI.xcframework/ios-arm64_x86_64-simulator/fediFFI.framework/fediFFI
 rm Sources/Fedi/fediFFI.h
 rm Sources/Fedi/fediFFI.modulemap
