c716c420f9e76a05ffd2fb1364d885d4bdd31fe2
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-08-25 09:53:33 -0700

    chore: in CI build only aarch64-apple-ios-sim (workaround)

--- scripts/bridge/build-bridge-ios.sh
+++ scripts/bridge/build-bridge-ios.sh
@@ -5,29 +5,34 @@ set -e
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 BRIDGE_ROOT=$REPO_ROOT/bridge
-cd $BRIDGE_ROOT
+TARGET_DIR=$REPO_ROOT/target
 
-TARGET=$REPO_ROOT/target
+cd $BRIDGE_ROOT
+# use the xcode shell to make sure we have the necessary SDKs
+if [ "${CARGO_PROFILE:-}" == "ci" ]; then
+  >&2 echo "Skipping x86_64-apple-ios and aarch64-apple-ios builds"
+else
+  cargo build --package fedi-ffi ${CARGO_PROFILE:+--profile ${CARGO_PROFILE}} --target x86_64-apple-ios $CARGO_FLAGS
+  cargo build --package fedi-ffi ${CARGO_PROFILE:+--profile ${CARGO_PROFILE}} --target aarch64-apple-ios $CARGO_FLAGS
+fi
+cargo build --package fedi-ffi ${CARGO_PROFILE:+--profile ${CARGO_PROFILE}} --target aarch64-apple-ios-sim $CARGO_FLAGS
+
+mkdir -p $TARGET_DIR/lipo-ios-sim/${CARGO_PROFILE:-debug}
+# shellcheck disable=SC2046
+lipo $(find $TARGET_DIR/ -name libfediffi.a | grep -v '/deps/') -create -output $TARGET_DIR/lipo-ios-sim/${CARGO_PROFILE:-debug}/libfediffi.a
 
 cd $BRIDGE_ROOT/fedi-ffi
 # note: using '--target-dir' or otherwise this build will completely invalidate previous ones already in the ./target
 cargo run --target-dir "${REPO_ROOT}/target/ffi-bindgen-run" --package ffi-bindgen -- --language swift --out-dir $BRIDGE_ROOT/fedi-swift/Sources/Fedi
-cd $BRIDGE_ROOT
-
-# use the xcode shell to make sure we have the necessary SDKs
-cargo build --package fedi-ffi ${CARGO_PROFILE:+--profile ${CARGO_PROFILE}} --target x86_64-apple-ios $CARGO_FLAGS
-cargo build --package fedi-ffi ${CARGO_PROFILE:+--profile ${CARGO_PROFILE}} --target aarch64-apple-ios $CARGO_FLAGS
-cargo build --package fedi-ffi ${CARGO_PROFILE:+--profile ${CARGO_PROFILE}} --target aarch64-apple-ios-sim $CARGO_FLAGS
-
-mkdir -p $TARGET/lipo-ios-sim/${CARGO_PROFILE:-debug}
-lipo $TARGET/aarch64-apple-ios-sim/${CARGO_PROFILE:-debug}/libfediffi.a ../target/x86_64-apple-ios/${CARGO_PROFILE:-debug}/libfediffi.a -create -output $TARGET/lipo-ios-sim/${CARGO_PROFILE:-debug}/libfediffi.a
 
 cd $BRIDGE_ROOT/fedi-swift
 mv Sources/Fedi/fedi.swift Sources/Fedi/Fedi.swift || true
 cp Sources/Fedi/fediFFI.h fediFFI.xcframework/ios-arm64/fediFFI.framework/Headers
 cp Sources/Fedi/fediFFI.h fediFFI.xcframework/ios-arm64_x86_64-simulator/fediFFI.framework/Headers
 cp Sources/Fedi/fediFFI.h fediFFI.xcframework/macos-arm64_x86_64/fediFFI.framework/Headers
-cp $TARGET/aarch64-apple-ios/${CARGO_PROFILE:-debug}/libfediffi.a fediFFI.xcframework/ios-arm64/fediFFI.framework/fediFFI
-cp $TARGET/lipo-ios-sim/${CARGO_PROFILE:-debug}/libfediffi.a fediFFI.xcframework/ios-arm64_x86_64-simulator/fediFFI.framework/fediFFI
+if [ -e "$TARGET_DIR/aarch64-apple-ios/${CARGO_PROFILE:-debug}/libfediffi.a" ]; then
+  cp $TARGET_DIR/aarch64-apple-ios/${CARGO_PROFILE:-debug}/libfediffi.a fediFFI.xcframework/ios-arm64/fediFFI.framework/fediFFI
+fi
+cp $TARGET_DIR/lipo-ios-sim/${CARGO_PROFILE:-debug}/libfediffi.a fediFFI.xcframework/ios-arm64_x86_64-simulator/fediFFI.framework/fediFFI
 rm Sources/Fedi/fediFFI.h
 rm Sources/Fedi/fediFFI.modulemap
