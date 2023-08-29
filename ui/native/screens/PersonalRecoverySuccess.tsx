82aa23ccd3abc48a320ab5f7acda4e101dad342f
oz <otech47@gmail.com>
2023-08-29 13:46:46 -0400

    refactor: go straight to initializing after recovery
    authenticatedMember will be checked there and redirect correctly

--- ui/native/screens/PersonalRecoverySuccess.tsx
+++ ui/native/screens/PersonalRecoverySuccess.tsx
@@ -2,10 +2,7 @@ import type { NativeStackScreenProps } from '@react-navigation/native-stack'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
-import { selectAuthenticatedMember } from '@fedi/common/redux'
-
 import Success from '../components/ui/Success'
-import { useAppSelector } from '../state/hooks'
 import type { RootStackParamList } from '../types/navigation'
 
 export type Props = NativeStackScreenProps<
@@ -15,14 +12,13 @@ export type Props = NativeStackScreenProps<
 
 const PersonalRecoverySuccess: React.FC<Props> = () => {
     const { t } = useTranslation()
-    const authenticatedMember = useAppSelector(selectAuthenticatedMember)
 
     return (
         <Success
             messageText={t('feature.recovery.you-completed-personal-recovery')}
             buttonText={t('words.okay')}
             // returning members might still need to set their username
-            nextScreen={authenticatedMember ? 'TabsNavigator' : 'Initializing'}
+            nextScreen={'Initializing'}
         />
     )
 }
