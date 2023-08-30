8180627380692ef877d3602944ef0175823bf6f6
oz <otech47@gmail.com>
2023-08-29 23:59:58 -0400

    fix: set env for pod install

--- scripts/ui/install-ios-deps.sh
+++ scripts/ui/install-ios-deps.sh
@@ -6,8 +6,9 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 $REPO_ROOT/scripts/enforce-nix.sh
 
 echo "Installing ios dependencies with Cocoapods"
-cd $REPO_ROOT/ui/native/ios
+pushd $REPO_ROOT/ui/native/ios
 # xcode shell ensures we have cocoapods installed
 nix develop .#xcode --command pod install
+popd
 
 echo "Finished installing iOS dependencies"
