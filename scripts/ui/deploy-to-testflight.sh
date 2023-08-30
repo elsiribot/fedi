be4121bc306ffdca31e98c592e70609612b5efe9
oz <otech47@gmail.com>
2023-08-29 21:42:56 -0400

    temp: use nonnix shell for deployment
    until we fix #1652

--- scripts/ui/deploy-to-testflight.sh
+++ scripts/ui/deploy-to-testflight.sh
@@ -7,6 +7,15 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 
 pushd $REPO_ROOT/ui/native/ios
 
+# Check if fastlane is installed
+if ! command -v fastlane &> /dev/null; then
+  echo "fastlane not found! Installing..."
+  # Installation steps for fastlane, assuming no dependencies
+  # You can replace this with the specific steps needed for your system
+  # For example, using RubyGems to install fastlane:
+  gem install fastlane -NV
+fi
+
 echo "Building Xcode release archive with fastlane (see $REPO_ROOT/ui/native/ios/Fastfile for lane configurations)..."
 fastlane beta_ci
 echo "Build complete!"
