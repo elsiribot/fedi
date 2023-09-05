5cc3d1ec574a44be28cf332287a3faa0e639795b
oz <otech47@gmail.com>
2023-09-04 22:59:52 -0400

    refactor: clean up nonnix fallback in script

--- scripts/ui/deploy-to-testflight.sh
+++ scripts/ui/deploy-to-testflight.sh
@@ -3,21 +3,16 @@
 set -e
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-# $REPO_ROOT/scripts/enforce-nix.sh
+$REPO_ROOT/scripts/enforce-nix.sh
 
 pushd $REPO_ROOT/ui/native/ios
 
-# Check if fastlane is installed
-if ! command -v fastlane &> /dev/null; then
-  echo "fastlane not found! Installing..."
-  # Installation steps for fastlane, assuming no dependencies
-  # You can replace this with the specific steps needed for your system
-  # For example, using RubyGems to install fastlane:
-  gem install fastlane -NV
-fi
+# Not sure if this step is necessary... Try uncommenting if CI starts failing
+# for keychain related reasons
+# security unlock-keychain -p $MATCH_KEYCHAIN_PASSWORD $MATCH_KEYCHAIN_NAME
 
 echo "Building Xcode release archive with fastlane (see $REPO_ROOT/ui/native/ios/Fastfile for lane configurations)..."
-fastlane beta_ci
+fastlane beta_ci --verbose
 echo "Build complete!"
 
 popd
