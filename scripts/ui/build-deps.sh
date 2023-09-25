89fe044dde76216254409db4face0f2e121b32f2
oz <otech47@gmail.com>
2023-09-25 14:44:29 +0200

    perf: dont build pwa as UI dep in CI

--- scripts/ui/build-deps.sh
+++ scripts/ui/build-deps.sh
@@ -5,13 +5,15 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 
 $REPO_ROOT/scripts/enforce-nix.sh
 
-cd $REPO_ROOT/ui
+pushd $REPO_ROOT/ui
+
 echo "Reinstalling node modules from lockfile (yarn.lock)"
 rm -rf $REPO_ROOT/ui/node_modules
 yarn install --frozen-lockfile
 echo "Finished installing node modules"
 
 echo "Building UI modules: @fedi/common and @fedi/injections"
-yarn build
-echo "Building UI modules: @fedi/common and @fedi/injections"
-cd $REPO_ROOT
+yarn build:deps
+echo "UI modules built: @fedi/common and @fedi/injections"
+
+popd
