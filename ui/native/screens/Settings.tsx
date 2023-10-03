a7434268e87aaf959602f39f921d02bc11d0b4a1
Will O'Beirne <wbobeirne@gmail.com>
2023-10-03 09:52:56 -0400

    fix(native): adjust omni component safe areas to be aware of the bottom tab navigation

--- ui/native/screens/Settings.tsx
+++ ui/native/screens/Settings.tsx
@@ -1,4 +1,3 @@
-import { BottomTabScreenProps } from '@react-navigation/bottom-tabs'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { Text, Theme, useTheme } from '@rneui/themed'
 import React, { useCallback, useState } from 'react'
