48cc377e12a05bafa4829bce4d2ecce998cb588c
Shaurya Arora <shaurya.arora1@gmail.com>
2023-10-09 15:51:20 -0400

    chore: exclude stability pool server module by default

--- fedimintd/src/main.rs
+++ fedimintd/src/main.rs
@@ -11,31 +11,37 @@ use stability_pool_server::StabilityPoolGen;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    Fedimintd::new()?
+    let include_stability_pool = std::env::var("INCLUDE_STABILITY_POOL").is_ok();
+
+    let mut fedimintd = Fedimintd::new()?
         .with_default_modules()
         .with_module(FediSocialGen)
-        .with_module(StabilityPoolGen)
-        .with_extra_module_inits_params(3, fedi_social_common::KIND, FediSocialGenParams::new())
-        .with_extra_module_inits_params(
-            4,
-            stability_pool_server::common::KIND,
-            StabilityPoolGenParams {
-                local: Default::default(),
-                consensus: StabilityPoolGenParamsConsensus {
-                    // oracle_config: OracleConfig::Aggregate, // switch oracle when not testing
-                    oracle_config: OracleConfig::Mock,
-                    cycle_duration: Duration::from_secs(15),
-                    collateral_ratio: CollateralRatio {
-                        provider: 1,
-                        seeker: 1,
+        .with_extra_module_inits_params(3, fedi_social_common::KIND, FediSocialGenParams::new());
+
+    if include_stability_pool {
+        fedimintd = fedimintd
+            .with_module(StabilityPoolGen)
+            .with_extra_module_inits_params(
+                4,
+                stability_pool_server::common::KIND,
+                StabilityPoolGenParams {
+                    local: Default::default(),
+                    consensus: StabilityPoolGenParamsConsensus {
+                        // oracle_config: OracleConfig::Aggregate, // switch oracle when not testing
+                        oracle_config: OracleConfig::Mock,
+                        cycle_duration: Duration::from_secs(15),
+                        collateral_ratio: CollateralRatio {
+                            provider: 1,
+                            seeker: 1,
+                        },
+                        min_allowed_seek: Amount::from_msats(100_000),
+                        min_allowed_provide: Amount::from_msats(100_000),
+                        max_allowed_provide_fee_rate_ppb: 2000,
+                        min_allowed_cancellation_bps: 100,
                     },
-                    min_allowed_seek: Amount::from_msats(100_000),
-                    min_allowed_provide: Amount::from_msats(100_000),
-                    max_allowed_provide_fee_rate_ppb: 2000,
-                    min_allowed_cancellation_bps: 100,
                 },
-            },
-        )
-        .run()
-        .await
+            );
+    }
+
+    fedimintd.run().await
 }
--- stability-pool/test.sh
+++ stability-pool/test.sh
@@ -3,6 +3,7 @@
 
 set -euo pipefail
 export RUST_LOG="${RUST_LOG:-info}"
+export INCLUDE_STABILITY_POOL=1
 source ../scripts/build.sh
 
 cargo test -p stability-pool-tests
