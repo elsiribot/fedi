81212232fc91f79fb65d67bb4abcb764c187dab9
oz <otech47@gmail.com>
2023-10-19 22:55:13 +0200

    refactor: versioning system tweaks
    to auto-deploy updates to app stores, we MUST increment the version
    number or app stores will reject the upload
    
    - make the build number timestamp based
    - move ios versioning out of fastlane into the same script as android
    - remove extra nix in just command

--- scripts/ui/bump-version-native.sh
+++ scripts/ui/bump-version-native.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Versioning
+# To bump the version number in /ui/native/package.json
+# we use `npm version` with either a minor or patch version bump
+# Then we use react-native-version to bump the version code
 set -e
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
@@ -15,11 +19,31 @@ fi
 
 pushd $REPO_ROOT/ui/native
 
-# Allow version bump if:
+# Build numbers are timestamp based to ensure they are always
+# increasing. Must be lower than 2,100,000,000 so the scheme is:
+PREFIX="1"
+# Get the last two digits of the year
+YY=$(date +"%y")
+# Get the day of the year, zero-padded
+DDD=$(date +"%j")
+# Get the current time in HHMM format
+HHMM=$(date +"%H%M")
+# Combine to form the build number
+BUILD_NUMBER="${PREFIX}${YY}${DDD}${HHMM}"
+
+# if we are building a nightly APK from the master branch in CI...
+# modify the build numbers so the app stores will accept
+# the upload. We do not commit this since build numbers are timestamp based
+if [[ -n $GITHUB_REF && $GITHUB_REF == refs/heads/master && -n $FLAVOR && $FLAVOR == "nightly" ]]; then
+  npx react-native-version --increment-build --never-amend --set-build $BUILD_NUMBER
+fi
+
+# Here we bump the version and commit to the repo, but only allow if:
 # - script is running locally
 # - script is running on a release branch in CI
 echo "Checking GITHUB_REF: $GITHUB_REF"
 if [[ -z $GITHUB_REF || $GITHUB_REF == refs/heads/release/* ]]; then
+  
   # Compare release branch with current version to determine
   # major vs minor vs patch version bump
   RELEASE_BRANCH_VERSION="${GITHUB_REF##*/}"
@@ -38,12 +62,20 @@ if [[ -z $GITHUB_REF || $GITHUB_REF == refs/heads/release/* ]]; then
 
   # app stores expect these version codes to increment so update
   # react native version numbers to match npm
-  echo "Bumping react-native version numbers to match npm"
-  npx react-native-version --target android
+  echo "Bumping Android version numbers to match npm"
+  npx react-native-version --set-build $BUILD_NUMBER --target android
+  # iOS: Navigate to xcode project and update version using agvtool
+  # using only the major.minor version to avoid excessive review times
+  # on Testflight. For the build number use react-native-version
+  echo "Bumping iOS version numbers to match npm"
+  pushd $REPO_ROOT/ui/native/ios
+  agvtool new-marketing-version $RELEASE_BRANCH_VERSION
+  npx react-native-version --increment-build --never-amend --set-build $BUILD_NUMBER
+  popd
   echo "Pushing version commit to git branch"
   NEW_VERSION="$(npm pkg get version  --ws false | sed 's/"//g')"
   echo "NEW_VERSION $NEW_VERSION"
-  git add package.json android/app/build.gradle && git commit -m "chore: bump version for ${NEW_VERSION}" && git push
+  git add package.json android/app/build.gradle ios/ && git commit -m "chore: bump version for ${NEW_VERSION}" && git push
 else
   echo "Not on a release branch. Don't push version commit."
 fi
