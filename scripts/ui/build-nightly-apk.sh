05e589ee4e7b60d4e315063e2d789d112ad1862b
oz <otech47@gmail.com>
2023-10-19 14:52:02 +0200

    build: unify deployment scripts
    with skippable configs
    
    and make sure nightly can be deployed to google play

--- scripts/ui/build-nightly-apk.sh
+++ scripts/ui/build-nightly-apk.sh
@@ -5,6 +5,23 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 
 $REPO_ROOT/scripts/enforce-nix.sh
 
+SKIP_BRIDGE_BUILD=${SKIP_BRIDGE_BUILD:-0}
+SKIP_UI_DEPS_BUILD=${SKIP_UI_DEPS_BUILD:-0}
+
+if [[ "$SKIP_BRIDGE_BUILD" == "1" ]]; then
+  echo "Skipping bridge build..."
+else
+  echo "Rebuilding Android bridge with release profile"
+  CARGO_PROFILE=release $REPO_ROOT/scripts/bridge/build-bridge-android.sh
+fi
+
+if [[ "$SKIP_UI_DEPS_BUILD" == "1" ]]; then
+  echo "Skipping UI dependencies build..."
+else
+  echo "Building UI dependencies..."
+  $REPO_ROOT/scripts/ui/build-deps.sh
+fi
+
 pushd $REPO_ROOT/ui/native/android
 
 echo "Building nightly flavor release APK with fastlane (see $REPO_ROOT/ui/native/android/Fastfile for lane configurations)..."
