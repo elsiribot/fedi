8d8d061794ac62709a8e53813bdfea12fd5242bf
Nitesh Balusu <84944042+niteshbalusu11@users.noreply.github.com>
2023-10-25 13:30:04 -0400

    fix: handle loading and error state for transactions
    Signed-off-by: Nitesh Balusu <84944042+niteshbalusu11@users.noreply.github.com>

--- ui/native/screens/Transactions.tsx
+++ ui/native/screens/Transactions.tsx
@@ -16,20 +16,28 @@ const Transactions: React.FC<Props> = () => {
     const { t } = useTranslation()
     const { listTransactions } = useBridge()
     const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState<Error | null>(null)
     // TODO: Hoist this into context so we can easily update individual
     // transactions and not have to refreshTransactions on every notes update
     const [transactionsList, setTransactionsList] = useState<Transaction[]>([])
 
     const getTransactionsList = useCallback(async () => {
-        const fetchedTransactions = await listTransactions()
-        console.info('fetchedTransactions', fetchedTransactions.length)
-        setTransactionsList(fetchedTransactions)
+        try {
+            setIsLoading(true)
+            const fetchedTransactions = await listTransactions()
+            console.info('fetchedTransactions', fetchedTransactions.length)
+            setTransactionsList(fetchedTransactions)
+            setError(null)
+        } catch (err: any) {
+            console.error('Failed to fetch transactions:', err)
+            setError(err)
+        } finally {
+            setIsLoading(false)
+        }
     }, [listTransactions])
 
     useEffect(() => {
-        setIsLoading(true)
         getTransactionsList()
-        setIsLoading(false)
     }, [getTransactionsList])
 
     if (isLoading) return <ActivityIndicator />
