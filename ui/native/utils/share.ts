513f20733d61f5337856a615b4607ffefe4c55e8
Will O'Beirne <wbobeirne@gmail.com>
2023-11-07 16:37:01 -0600

    feat(native): add info.json to export with various device information

--- ui/native/utils/share.ts
+++ ui/native/utils/share.ts
@@ -4,12 +4,20 @@ import Share from 'react-native-share'
 import { exportLogs } from '@fedi/common/utils/log'
 import { makeTarGz } from '@fedi/common/utils/targz'
 
+import { getAllDeviceInfo } from './device-info'
+
 export async function shareLogs() {
-    const jsLogs = await exportLogs()
-    const bridgeLogs = await RNFS.read(`${RNFS.DocumentDirectoryPath}/fedi.log`)
+    // Parallelize all information gathering.
+    const [jsLogs, bridgeLogs, infoJson] = await Promise.all([
+        exportLogs(),
+        RNFS.read(`${RNFS.DocumentDirectoryPath}/fedi.log`),
+        getAllDeviceInfo(),
+    ])
+
     const targz = await makeTarGz([
         { name: 'app.log', content: jsLogs },
         { name: 'bridge.log', content: bridgeLogs },
+        { name: 'info.json', content: JSON.stringify(infoJson, null, 2) },
     ])
     const filename = `fedi-logs-${Math.floor(Date.now() / 1000)}.tar.gz`
     return Share.open({
