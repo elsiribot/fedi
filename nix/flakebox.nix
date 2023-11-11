0dcb8c9f00245548d25e1cf3d3204a83f00a1c6a
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-11-10 18:04:14 -0800

    fix: don't modify PATH from scripts/build.sh

--- nix/flakebox.nix
+++ nix/flakebox.nix
@@ -239,7 +239,7 @@ rec {
       for i in lnd lightningd gatewayd devimint esplora electrs bitcoind faucet ; do
          which $i
       done
-      ./scripts/test-bridge-v1.sh
+      ./scripts/test-bridge-v1.inner.sh
     '';
   };
 
