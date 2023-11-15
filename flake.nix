461a091e55963c98c9f91faa041cbc8401e0391b
Justin Moon <mail@justinmoon.com>
2023-11-15 16:18:09 -0600

    fix: use correct version of lightningd in v0 dev shell

--- flake.nix
+++ flake.nix
@@ -320,6 +320,14 @@
           });
           v0 = fedi-v1.devShells.${system}.default.overrideAttrs (prev: {
             nativeBuildInputs = [
+              (
+                (import fedi-v0.inputs.nixpkgs { inherit system; }).clightning.overrideAttrs
+                  (oldAttrs: {
+                    configureFlags = [ "--enable-developer" "--disable-valgrind" ];
+                  } // pkgs.lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+                    NIX_CFLAGS_COMPILE = "-Wno-stringop-truncation";
+                  })
+              )
               fedi-v0.inputs.fedimint-build.packages.${system}.devimint
               fedi-v0.inputs.fedimint-pkgs.packages.${system}.gateway-pkgs
               fedi-v0.inputs.fedimint-pkgs.packages.${system}.fedimint-dbtool-pkgs
