47937937008f5650e560b36afebe196435b37e12
Nitesh Balusu <84944042+niteshbalusu11@users.noreply.github.com>
2023-11-15 09:43:36 -0500

    fix remove default max for invoice and balance
    Signed-off-by: Nitesh Balusu <84944042+niteshbalusu11@users.noreply.github.com>

--- ui/common/redux/federation.ts
+++ ui/common/redux/federation.ts
@@ -341,8 +341,8 @@ export const selectFederationCustomFediMods = (s: CommonState) => {
 // For now we set a safe default of 200K sats maximum unless otherwise
 // specified by the federation feature flags. At some points we probably
 // can remove this hard-coded value altogether
-const MAX_INVOICE_AMOUNT_SATS = 200000 as Sats
-const MAX_BALANCE_AMOUNT_SATS = 200000 as Sats
+const MAX_INVOICE_AMOUNT_SATS = 100000000 as Sats
+const MAX_BALANCE_AMOUNT_SATS = 100000000 as Sats
 
 export const selectMaxReceiveAmount = createSelector(
     selectFederationMetadata,
