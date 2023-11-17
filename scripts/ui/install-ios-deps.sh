9eafd9b7aeb6bddd6af6e1d5af25376cb6ab3238
Justin Moon <mail@justinmoon.com>
2023-11-17 15:22:51 -0600

    fix: mitigate glog compilation failure

--- scripts/ui/install-ios-deps.sh
+++ scripts/ui/install-ios-deps.sh
@@ -8,7 +8,8 @@ $REPO_ROOT/scripts/enforce-nix.sh
 echo "Installing ios dependencies with Cocoapods"
 pushd $REPO_ROOT/ui/native/ios
 # xcode shell ensures we have cocoapods installed
-nix develop .#xcode --command pod install --repo-update
+# running `pod install --repo-update` twice because of https://github.com/fedibtc/fedi/issues/2459
+nix develop .#xcode --command bash -c 'pod install --repo-update || pod install --repo-update'
 popd
 
 echo "Finished installing iOS dependencies"
