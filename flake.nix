002c88b7dfac24e7d9a6e4ad5135bfb8f1fa32f8
Dawid Ciężarkiewicz <dpc@dpc.pw>
2023-12-08 17:50:09 -0800

    chore: include a devimint fix
    https://github.com/fedimint/fedimint/pull/3823/commits/4d84c957dd751a9fb055f18c19461e6cd19cadc0
    
    from https://github.com/fedimint/fedimint/pull/3823

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     flake-utils.url = "github:numtide/flake-utils";
     # we pick upstream packages from here, so we want this to be compatible with our forks
     fedimint-pkgs = {
-      url = "git+https://x-access-token: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@github.com/fedibtc/fedimint-fedi.git?ref=shaurya/v0.2.0-rc7&rev=5af6351ca2a45eb599e860fd39e7172e112de8a0";
+      url = "git+https://x-access-token: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@github.com/fedibtc/fedimint-fedi.git?ref=dpc/v0.2.0-rc-7-devimint-fix&rev=9882d47b2b252218db93eafa6b4160dca554d2f9";
     };
     # TODO shaurya can probably remove once bridge is updated for 0.2
     # Fedimint at consensus version 1. This is used to test bridge against old federations
