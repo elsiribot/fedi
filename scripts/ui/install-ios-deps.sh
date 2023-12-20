a496debe7c49e85e68223c28004aeb1f80c142f9
oz <otech47@gmail.com>
2023-12-20 14:15:04 -0500

    fix: ensure clean pods directory when installing ios deps from CI

--- scripts/ui/install-ios-deps.sh
+++ scripts/ui/install-ios-deps.sh
@@ -6,10 +6,19 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 $REPO_ROOT/scripts/enforce-nix.sh
 
 echo "Installing ios dependencies with Cocoapods"
+
 pushd $REPO_ROOT/ui/native/ios
-# xcode shell ensures we have cocoapods installed
-# running `pod install --repo-update` twice because of https://github.com/fedibtc/fedi/issues/2459
-nix develop .#xcode --command bash -c 'pod install --repo-update || pod install --repo-update'
+
+if [[ -n "$CI" ]]; then
+  echo "Deleting & deintegrating Pods for fresh install..."
+  rm -rf $REPO_ROOT/ui/native/ios/Pods
+  pod deintegrate
+  pod install --repo-update
+  pod update
+else
+  pod install --repo-update
+fi
+
 popd
 
 echo "Finished installing iOS dependencies"
