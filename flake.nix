adea1125347e572ff0c1c2d055fe05e005026ab5
oz <otech47@gmail.com>
2023-12-20 14:15:43 -0500

    fix: set macos deployment target to avoid glog error on pod install

--- flake.nix
+++ flake.nix
@@ -325,6 +325,7 @@
               # to use ld instead of clang for linking the bridge binary
               export LD=/usr/bin/clang
               export LD_FOR_TARGET=/usr/bin/clang
+              export MACOSX_DEPLOYMENT_TARGET=""
             '';
           });
           v1 = fedi-v1.devShells.${system}.default.overrideAttrs (prev: {
