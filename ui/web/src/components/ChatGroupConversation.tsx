f6dd2604df12fe25c8c5ad9af08150dea6cc1fe8
Conner Ow <connerow1115@gmail.com>
2023-12-20 13:23:04 -0600

    feat: PWA Option to leave chat group

--- ui/web/src/components/ChatGroupConversation.tsx
+++ ui/web/src/components/ChatGroupConversation.tsx
@@ -1,10 +1,12 @@
 import React, { useCallback, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
-import EditIcon from '@fedi/common/assets/svgs/edit.svg'
+import CogIcon from '@fedi/common/assets/svgs/cog.svg'
+import LeaveRoom from '@fedi/common/assets/svgs/leave-room.svg'
 import QRIcon from '@fedi/common/assets/svgs/qr.svg'
 import {
     configureChatGroup,
+    leaveChatGroup,
     selectActiveFederationId,
     selectChatGroup,
     selectChatGroupRole,
@@ -15,14 +17,17 @@ import { ChatRole, ChatType } from '@fedi/common/types'
 import { encodeGroupInvitationLink } from '@fedi/common/utils/xmpp'
 
 import { useAppDispatch, useAppSelector, useToast } from '../hooks'
-import { styled } from '../styles'
+import { styled, theme } from '../styles'
+import { Button } from './Button'
 import { ChatConversation } from './ChatConversation'
 import { ChatEmptyState } from './ChatEmptyState'
 import { CopyInput } from './CopyInput'
 import { Dialog } from './Dialog'
 import { IconButton } from './IconButton'
+import { Input } from './Input'
 import * as Layout from './Layout'
 import { QRCode } from './QRCode'
+import { Text } from './Text'
 
 interface Props {
     groupId: string
@@ -36,7 +41,9 @@ export const ChatGroupConversation: React.FC<Props> = ({ groupId }) => {
     const group = useAppSelector(s => selectChatGroup(s, groupId))
     const messages = useAppSelector(s => selectChatMessages(s, groupId))
     const role = useAppSelector(s => selectChatGroupRole(s, groupId))
-    const [isDialogOpen, setIsDialogOpen] = useState(false)
+    const [isShareDialogOpen, setIsShareDialogOpen] = useState(false)
+    const [isSettingsDialogOpen, setIsSettingsDialogOpen] = useState(false)
+    const [groupName, setGroupName] = useState(group?.name || '')
 
     const handleSend = useCallback(
         async (content: string) => {
@@ -56,19 +63,28 @@ export const ChatGroupConversation: React.FC<Props> = ({ groupId }) => {
     const handleEditGroupName = useCallback(async () => {
         try {
             if (!federationId || !group) return
-            const newName = prompt(t('feature.chat.change-group-name'))
-            if (!newName) return
+            if (!groupName) return
             await dispatch(
                 configureChatGroup({
                     federationId,
                     groupId: group?.id,
-                    groupName: newName,
+                    groupName,
                 }),
             ).unwrap()
+            setIsSettingsDialogOpen(false)
         } catch (err) {
             showErrorToast(err, 'errors.unknown-error')
         }
-    }, [t, dispatch, showErrorToast, federationId, group])
+    }, [dispatch, showErrorToast, federationId, group, groupName])
+
+    const handleLeaveGroup = useCallback(async () => {
+        const shouldLeave = confirm(t('feature.chat.leave-group-confirmation'))
+        if (!federationId || !group) return
+        if (shouldLeave) {
+            await dispatch(leaveChatGroup({ federationId, groupId: group?.id }))
+            setIsSettingsDialogOpen(false)
+        }
+    }, [t, federationId, group, dispatch])
 
     if (!group) {
         return (
@@ -87,21 +103,19 @@ export const ChatGroupConversation: React.FC<Props> = ({ groupId }) => {
             headerActions={
                 group && (
                     <>
-                        {role === ChatRole.moderator && (
-                            <IconButton
-                                size="md"
-                                icon={EditIcon}
-                                onClick={handleEditGroupName}
-                            />
-                        )}
                         <IconButton
                             size="md"
                             icon={QRIcon}
-                            onClick={() => setIsDialogOpen(true)}
+                            onClick={() => setIsShareDialogOpen(true)}
+                        />
+                        <IconButton
+                            size="md"
+                            icon={CogIcon}
+                            onClick={() => setIsSettingsDialogOpen(true)}
                         />
                         <Dialog
-                            open={isDialogOpen}
-                            onOpenChange={setIsDialogOpen}
+                            open={isShareDialogOpen}
+                            onOpenChange={setIsShareDialogOpen}
                             title={t('feature.chat.invite-to-group')}>
                             <Layout.Root>
                                 <Layout.Content centered>
@@ -119,6 +133,52 @@ export const ChatGroupConversation: React.FC<Props> = ({ groupId }) => {
                                 </Layout.Actions>
                             </Layout.Root>
                         </Dialog>
+                        <Dialog
+                            open={isSettingsDialogOpen}
+                            onOpenChange={setIsSettingsDialogOpen}
+                            title={group.name}>
+                            <Layout.Root>
+                                <Layout.Content centered>
+                                    <SettingsContentWrapper>
+                                        {role === ChatRole.moderator && (
+                                            <RenameWrapper>
+                                                <Input
+                                                    value={groupName}
+                                                    onChange={e =>
+                                                        setGroupName(
+                                                            e.target.value,
+                                                        )
+                                                    }
+                                                    name="group-name"
+                                                    placeholder={t(
+                                                        'feature.chat.group-name',
+                                                    )}
+                                                    label={
+                                                        <Text variant="caption">
+                                                            {t(
+                                                                'feature.chat.group-name',
+                                                            )}
+                                                        </Text>
+                                                    }
+                                                />
+                                                <Button
+                                                    onClick={
+                                                        handleEditGroupName
+                                                    }>
+                                                    {t('words.save')}
+                                                </Button>
+                                            </RenameWrapper>
+                                        )}
+                                        <Button
+                                            onClick={handleLeaveGroup}
+                                            variant="outline"
+                                            icon={LeaveRoom}>
+                                            {t('feature.chat.leave-group')}
+                                        </Button>
+                                    </SettingsContentWrapper>
+                                </Layout.Content>
+                            </Layout.Root>
+                        </Dialog>
                     </>
                 )
             }
@@ -129,4 +189,16 @@ export const ChatGroupConversation: React.FC<Props> = ({ groupId }) => {
 const QRWrapper = styled('div', {
     width: '100%',
     margin: '12px auto 0',
+})
+
+const RenameWrapper = styled('div', {
+    display: 'flex',
+    gap: theme.space.md,
+    alignItems: 'end',
+})
+
+const SettingsContentWrapper = styled('div', {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: theme.space.md,
 })
