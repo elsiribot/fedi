d9289f85312c780f55b230883d7d6ae2ecc18e6f
Conner Ow <connerow1115@gmail.com>
2024-01-11 15:47:41 -0600

    fix: Remove hardcoded Federation Icons

--- ui/native/components/ui/FederationLogo.tsx
+++ ui/native/components/ui/FederationLogo.tsx
@@ -4,7 +4,7 @@ import { Image } from 'react-native'
 import { Federation } from '@fedi/common/types'
 import { getFederationIconUrl } from '@fedi/common/utils/FederationUtils'
 
-import SvgImage, { SvgImageName, SvgImageSize } from './SvgImage'
+import { SvgImageSize } from './SvgImage'
 
 type Props = {
     federation?: Pick<Federation, 'id' | 'name' | 'meta'>
@@ -15,24 +15,10 @@ export const FederationLogo: React.FC<Props> = ({ federation, size }) => {
     const iconUrl = federation?.meta
         ? getFederationIconUrl(federation?.meta)
         : null
-    const svgSize = typeof size !== 'number' ? size : undefined
     const svgProps =
         typeof size === 'number' ? { width: size, height: size } : undefined
 
-    let name: SvgImageName = 'Federation'
-    if (federation?.name.toLowerCase().includes('alpha')) {
-        name = 'FederationAlphaIcon'
-    } else if (federation?.name.toLowerCase().includes('prague')) {
-        name = 'FederationPragueIcon'
-    }
-
     return iconUrl ? (
         <Image style={svgProps} source={{ uri: iconUrl }} resizeMode="cover" />
-    ) : (
-        <SvgImage
-            name={name}
-            size={svgSize}
-            svgProps={{ stroke: 'transparent', ...svgProps }}
-        />
-    )
+    ) : null
 }
--- ui/web/src/components/FederationAvatar.tsx
+++ ui/web/src/components/FederationAvatar.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import FederationAlphaImage from '@fedi/common/assets/images/federation-alpha.png'
-import FederationPragueImage from '@fedi/common/assets/images/federation-prague.png'
 import { Federation } from '@fedi/common/types'
 import { getFederationIconUrl } from '@fedi/common/utils/FederationUtils'
 
@@ -12,25 +10,15 @@ type Props = Omit<AvatarProps, 'id' | 'shape' | 'name'> & {
 }
 
 export const FederationAvatar: React.FC<Props> = ({ federation, ...props }) => {
-    let src: string | undefined
     const iconUrl = getFederationIconUrl(federation.meta)
 
-    // TODO: pull icon from federation meta, not hard coded name matching
-    if (iconUrl) {
-        src = iconUrl
-    } else if (federation.name.toLowerCase().includes('alpha')) {
-        src = FederationAlphaImage.src
-    } else if (federation.name.toLowerCase().includes('prague')) {
-        src = FederationPragueImage.src
-    }
-
-    return (
+    return iconUrl ? (
         <Avatar
             id={federation.id}
             shape="square"
-            src={src}
+            src={iconUrl}
             name={federation.name}
             {...props}
         />
-    )
+    ) : null
 }
