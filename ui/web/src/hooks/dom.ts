768ef87992d5a485026c0d25836836693fd03505
Conner Ow <connerow1115@gmail.com>
2024-01-12 16:50:55 -0600

    fix: BeforeUnload / Confirmation (PWA)

--- ui/web/src/hooks/dom.ts
+++ ui/web/src/hooks/dom.ts
@@ -1,3 +1,4 @@
+import Router from 'next/router'
 import { useEffect } from 'react'
 
 export const useAutosizeTextArea = (
@@ -22,4 +23,35 @@ export const useIsTouchScreen = () => {
         'ontouchstart' in window ||
         ('maxTouchPoints' in navigator && navigator.maxTouchPoints > 0)
     )
+}
+
+export const useWarnBeforeUnload = (shouldWarn: boolean) => {
+    useEffect(() => {
+        const confirmationMessage = 'Changes you made may not be saved.'
+
+        const beforeUnloadHandler = (e: BeforeUnloadEvent) => {
+            ;(e || window.event).returnValue = confirmationMessage
+            return confirmationMessage
+        }
+
+        const beforeRouteHandler = (url: string) => {
+            if (Router.pathname !== url && !confirm(confirmationMessage)) {
+                Router.events.emit('routeChangeError')
+                throw `Route change to "${url}" was aborted (this error can be safely ignored). See https://github.com/zeit/next.js/issues/2476.`
+            }
+        }
+
+        if (shouldWarn) {
+            window.addEventListener('beforeunload', beforeUnloadHandler)
+            Router.events.on('routeChangeStart', beforeRouteHandler)
+        } else {
+            window.removeEventListener('beforeunload', beforeUnloadHandler)
+            Router.events.off('routeChangeStart', beforeRouteHandler)
+        }
+
+        return () => {
+            window.removeEventListener('beforeunload', beforeUnloadHandler)
+            Router.events.off('routeChangeStart', beforeRouteHandler)
+        }
+    }, [shouldWarn])
 }
