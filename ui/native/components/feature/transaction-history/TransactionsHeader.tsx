02e745e4afdca6321b5b9d5c14ac1f2bef5333b0
Conner Ow <connerow1115@gmail.com>
2024-01-22 10:00:08 -0600

    fix: (Native) Transactions close button > back button

--- ui/native/components/feature/transaction-history/TransactionsHeader.tsx
+++ ui/native/components/feature/transaction-history/TransactionsHeader.tsx
@@ -1,33 +1,19 @@
-import { useNavigation } from '@react-navigation/native'
-import { Text, useTheme } from '@rneui/themed'
+import { Text } from '@rneui/themed'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { Pressable } from 'react-native'
 
 import Header from '../../ui/Header'
-import SvgImage from '../../ui/SvgImage'
 
 const TransactionsHeader: React.FC = () => {
-    const { theme } = useTheme()
     const { t } = useTranslation()
-    const navigation = useNavigation()
 
     return (
         <Header
+            backButton
             headerCenter={
                 <Text bold numberOfLines={1} adjustsFontSizeToFit>
                     {t('words.transactions')}
                 </Text>
-            }
-            headerRight={
-                <Pressable
-                    onPress={() => navigation.goBack()}
-                    hitSlop={5}
-                    style={{
-                        padding: theme.spacing.sm,
-                    }}>
-                    <SvgImage name="Close" />
-                </Pressable>
             }
         />
     )
