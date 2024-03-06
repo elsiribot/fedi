30796cd47971e91cbad693724a77dd665bcdfa39
Shaurya Arora <shaurya.arora1@gmail.com>
2024-03-06 16:53:54 -0500

    chore: regenerate WASM bundle

--- ui/common/wasm/fedi_wasm.d.ts
+++ ui/common/wasm/fedi_wasm.d.ts
@@ -54,7 +54,7 @@ export interface InitOutput {
   readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
   readonly __wbindgen_export_2: WebAssembly.Table;
   readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
-  readonly wasm_bindgen__convert__closures__invoke1_mut__h882872ca37c8dc40: (a: number, b: number, c: number, d: number) => void;
+  readonly wasm_bindgen__convert__closures__invoke1_mut__h105a69bbe06c8fa2: (a: number, b: number, c: number, d: number) => void;
   readonly wasm_bindgen__convert__closures__invoke1_mut__hbc4c5b1dabebcb54: (a: number, b: number, c: number, d: number) => void;
   readonly wasm_bindgen__convert__closures__invoke0_mut__h8c2c9ebc2a4c22bd: (a: number, b: number) => void;
   readonly wasm_bindgen__convert__closures__invoke1_mut__h05d182073cf7a734: (a: number, b: number, c: number) => void;
