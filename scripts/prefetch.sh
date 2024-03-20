a0d9cb0f3c87d22784fc1ce36036b3facedfcdff
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-03-20 14:25:51 -0700

    feat: `just prefetch`

---
+++ scripts/prefetch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+source scripts/common.sh
+
+remote="${1:-upstream}"
+branch="${2:-master}"
+
+nix_system="$(nix eval --raw --impure --expr builtins.currentSystem)"
+
+git fetch "$remote" "$branch"
+
+repo="git+file:${REPO_ROOT}?ref=refs/remotes/$remote/$branch"
+nix build "$repo#devShells.${nix_system}.default"
