dca8662692c47be4380f3dabbc1470ad51d12272
Conner Ow <connerow1115@gmail.com>
2024-03-21 16:53:30 -0500

    fix: android toggle switches

--- ui/native/styles/theme.ts
+++ ui/native/styles/theme.ts
@@ -222,6 +222,12 @@ const theme = createTheme({
                 fontFamily: 'AlbertSans-Regular',
             },
         },
+        Switch: {
+            trackColor: {
+                false: colors.lightGrey,
+                true: colors.lightGrey,
+            },
+        },
         Header: {
             containerStyle: {
                 paddingHorizontal: 16,
