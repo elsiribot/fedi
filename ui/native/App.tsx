bc7719c242e7309309885c306c5aa248798dff6a
oz <otech47@gmail.com>
2024-03-22 22:47:13 -0400

    fix: make sure to hide the splash screen if bridge panics
    we should capture failures and show the error screen where logs can be uploaded

--- ui/native/App.tsx
+++ ui/native/App.tsx
@@ -6,6 +6,7 @@ import { useTranslation } from 'react-i18next'
 import { Platform } from 'react-native'
 import RNFS from 'react-native-fs'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
+import SplashScreen from 'react-native-splash-screen'
 import { Provider as ReduxProvider } from 'react-redux'
 
 import { ErrorBoundary } from '@fedi/common/components/ErrorBoundary'
@@ -116,6 +117,7 @@ const App = () => {
         const unsubscribePanic = fedimint.addListener('panic', event => {
             log.error('bridge panic', event)
             setBridgeError(event)
+            SplashScreen.hide()
         })
 
         return () => {
