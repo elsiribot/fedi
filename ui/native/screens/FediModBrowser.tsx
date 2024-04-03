c7b12b14760d52e980ca2ad84fe274dfa8907b8c
Conner Ow <connerow1115@gmail.com>
2024-04-03 09:37:25 -0500

    fix: ci

--- ui/native/screens/FediModBrowser.tsx
+++ ui/native/screens/FediModBrowser.tsx
@@ -4,7 +4,6 @@ import React, { MutableRefObject, useEffect, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import {
     ActivityIndicator,
-    Alert,
     BackHandler,
     Linking,
     StyleSheet,
