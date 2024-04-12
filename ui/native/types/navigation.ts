9115518cd568d9e6a717feb40a91724471516065
oz <otech47@gmail.com>
2024-04-11 21:38:31 -0400

    chore: add headers and nav types for seed reuse screens

--- ui/native/types/navigation.ts
+++ ui/native/types/navigation.ts
@@ -97,6 +97,10 @@ export type RootStackParamList = {
     ReceiveOffline: undefined
     RecoveryWords: undefined
     RecoveryAssistSuccess: undefined
+    RecoveryWalletOptions: undefined
+    RecoveryWalletTransfer: undefined
+    RecoveryNewWallet: undefined
+    RecoveryDeviceSelection: undefined
     RecordBackupVideo: undefined
     GroupChat: { groupId: string }
     GroupAdmin: { roomId: string }
