4b11c2a9b51abe90b4971bc6766637519e4bf196
Shaurya Arora <shaurya.arora1@gmail.com>
2024-04-16 22:54:36 -0400

    Revert "feat: check for deviceIndex is none in register device index"
    This reverts commit 88b38540bf42c09d21a8a27a6d00604031ed3208.

--- bridge/fedi-ffi/src/bridge.rs
+++ bridge/fedi-ffi/src/bridge.rs
@@ -727,7 +727,6 @@ impl Bridge {
         index: u8,
         force_overwrite: bool,
     ) -> anyhow::Result<Option<RpcFederation>> {
-        self.check_device_index_unassigned().await?;
         let seed = self.app_state.root_mnemonic().await;
         let identifier = self
             .app_state
@@ -781,16 +780,6 @@ impl Bridge {
         } else {
             Ok(None)
         }
-    }
-
-    async fn check_device_index_unassigned(&self) -> anyhow::Result<()> {
-        anyhow::ensure!(
-            self.app_state
-                .with_read_lock(|state| state.device_index.is_none())
-                .await,
-            "device index is already assigned"
-        );
-        Ok(())
     }
 
     pub async fn device_index_assignment_status(
