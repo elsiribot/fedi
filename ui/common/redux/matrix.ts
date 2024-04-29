fd71e03324f46b0d535bc837896aa1f7366d296e
Alex Lewin <alexlwn123@gmail.com>
2024-04-29 13:54:20 -0400

    chore: cleanup unused selector

--- ui/common/redux/matrix.ts
+++ ui/common/redux/matrix.ts
@@ -1000,11 +1000,6 @@ export const selectMatrixDirectMessageRoom = createSelector(
     (userId, rooms) => rooms.find(room => room.directUserId === userId),
 )
 
-export const selectMatrixDirectMessageRooms = createSelector(
-    selectMatrixRooms,
-    roomList => roomList.filter(room => !!room.directUserId),
-)
-
 export const selectMatrixHasNotifications = createSelector(
     selectMatrixRooms,
     rooms => rooms.some(room => room.notificationCount > 0),
