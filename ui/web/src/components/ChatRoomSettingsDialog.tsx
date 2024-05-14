767a4aef02ef1613f8477085f784f1dec09e49f2
oz <otech47@gmail.com>
2024-05-14 11:52:09 +0200

    feat(pwa): matrix default groups
    - just join on bridge initialize and add disable conditions for invite/leave buttons

--- ui/web/src/components/ChatRoomSettingsDialog.tsx
+++ ui/web/src/components/ChatRoomSettingsDialog.tsx
@@ -13,6 +13,7 @@ import {
     selectMatrixRoomSelfPowerLevel,
     setMatrixRoomBroadcastOnly,
     setMatrixRoomName,
+    selectAllDefaultMatrixRooms,
 } from '@fedi/common/redux'
 import { MatrixPowerLevel, MatrixRoom } from '@fedi/common/types'
 
@@ -51,6 +52,9 @@ export const ChatRoomSettingsDialog: React.FC<Props> = ({
     const { error } = useToast()
 
     const isAdmin = myPowerLevel >= MatrixPowerLevel.Admin
+    const isDefaultGroup = useAppSelector(s =>
+        selectAllDefaultMatrixRooms(s).includes(room?.id || ''),
+    )
 
     useEffect(() => {
         if (open) return
@@ -124,11 +128,13 @@ export const ChatRoomSettingsDialog: React.FC<Props> = ({
                         label: t('feature.chat.invite-to-group'),
                         icon: RoomIcon,
                         onClick: () => setPage('invite'),
+                        disabled: !isAdmin,
                     },
                     {
                         label: t('feature.chat.leave-group'),
                         icon: LeaveRoomIcon,
                         onClick: handleLeaveRoom,
+                        disabled: isDefaultGroup,
                     },
                     {
                         label: t('feature.chat.change-group-name'),
