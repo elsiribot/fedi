230bb284f90c7754bb9a1a068439f240e492e334
oz <otech47@gmail.com>
2024-08-28 09:35:18 -0400

    fix: tweak 1hr old address filter
    - since onchainState will no longer be provided for old 0.3 operations where no deposit was made

--- ui/common/redux/transactions.ts
+++ ui/common/redux/transactions.ts
@@ -200,8 +200,10 @@ export const selectTransactionHistory = createSelector(
             if (
                 txn.bitcoin &&
                 txn.direction === TransactionDirection.receive &&
-                txn.onchainState?.type === 'waitingForTransaction' &&
-                Date.now() / 1000 - txn.createdAt > 3600
+                Date.now() / 1000 - txn.createdAt > 3600 &&
+                (!txn.onchainState ||
+                    (txn.onchainState.type !== 'waitingForConfirmation' &&
+                        txn.onchainState.type !== 'claimed'))
             ) {
                 return false
             }
