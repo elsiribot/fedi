56af104bdc7273728a161518fd6a3839de2adf96
maan2003 <manmeetmann2003@gmail.com>
2024-09-24 16:58:43 +0530

    bump: fedimint 0.4.3-rc.1-fed1

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     fedimint-pkgs = {
-      url = "github:fedibtc/fedimint?ref=v0.4.2-rc.4-fed1";
+      url = "github:fedibtc/fedimint?ref=v0.4.3-rc.1-fed1";
     };
 
     fenix = {
