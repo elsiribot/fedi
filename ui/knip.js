3d681f42c3475a52431cf6dd5ad272aadeefea2a
oz <otech47@gmail.com>
2024-11-15 00:24:05 +0100

    Revert "chore: convert knip.js to knip.ts"
    This reverts commit 698f2e5e618c54d795176806ea9fdc19dd46bef0.

--- ui/knip.js
+++ ui/knip.js
@@ -1,13 +1,13 @@
-import type { KnipConfig } from 'knip'
-const config: KnipConfig = {
-    ignoreDependencies: ['@fedi/common'],
+/** @type {import('knip').KnipConfig} */
+const config = {
+    ignoreDependencies: ['@fedi/common/*'],
     ignoreExportsUsedInFile: true,
     ignore: ['./web/.next'],
     ignoreMembers: ['SupportedCurrency'],
     rules: {
         enumMembers: 'off',
         classMembers: 'off',
-    },
-}
+    }
+};
 
-export default config
+export default config;
