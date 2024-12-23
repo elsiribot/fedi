b969fdbd99732e86ba4aaf94c8a954a42a5d4d07
Dawid Ciężarkiewicz <dpc@dpc.pw>
2024-12-23 10:51:59 -0800

    chore: update wasm-bindgen to 0.2.99

--- flake.nix
+++ flake.nix
@@ -52,7 +52,11 @@
               convco = pkgs-unstable.convco;
               cargo-deluxe = cargo-deluxe.packages.${system}.default;
               wasm-pack = pkgs-unstable.wasm-pack;
-              wasm-bindgen-cli = pkgs-unstable.wasm-bindgen-cli;
+              wasm-bindgen-cli = pkgs-unstable.wasm-bindgen-cli.override {
+                version = "0.2.99";
+                hash = "sha256-1AN2E9t/lZhbXdVznhTcniy+7ZzlaEp/gwLEAucs6EA=";
+                cargoHash = "sha256-DbwAh8RJtW38LJp+J9Ht8fAROK9OabaJ85D9C/Vkve4=";
+              };
               binaryen = pkgs-unstable.binaryen;
               snappy = prev.snappy.overrideAttrs (f: p: rec {
                 version = "1.2.1";
