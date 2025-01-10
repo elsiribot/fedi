1f882aea1c9d0849d7322013f36a1723f74c8211
Dawid Ciężarkiewicz <dpc@dpc.pw>
2025-01-10 14:23:53 -0800

    chore(ci): update to self-unlocking target dir
    Existing version of `fs-dir-cache` uses an explicit unlocking
    command + timeout to unlock the target dir for other builds.
    This sometimes lead to unlocking not actually happening
    (e.g. job abruptly terminated) and next building having to wait
    until the timeout expires.
    
    New version of fs-dir-cache uses a unix socket that will automatically
    get unlocked when the process dies, so there is no to leave behind
    unlocked directory.

--- scripts/ci/run-in-fs-dir-cache.sh
+++ scripts/ci/run-in-fs-dir-cache.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
- 
+
 job_name="$1"
 shift 1
 
@@ -17,27 +17,36 @@ export FS_DIR_CACHE_LOCK_TIMEOUT_SECS="$((60 * 60))" # unlock after timeout in c
 
 log_file="$FS_DIR_CACHE_ROOT/log"
 
-fs-dir-cache gc unused --seconds "$((3 * 24 * 60 * 60))" # delete caches not used in more than a 5 days
+fs-dir-cache gc unused --seconds "$((3 * 24 * 60 * 60))" # delete caches not used in more than a 3 days
 
-# create/reuse cache (sub-directory) and lock it (wait if already locked)
-cache_dir=$(fs-dir-cache lock --key-file Cargo.lock --key-str "${CARGO_PROFILE-:dev}" --key-file flake.lock)
+export log_file
+export job_name
+src_dir=$(pwd)
+export src_dir
 
-export TARGET_DIR="$cache_dir/target"
-export CARGO_BUILD_TARGET_DIR="$TARGET_DIR"
+function run_in_cache() {
+    echo "$(date --rfc-3339=seconds) RUN job=$job_name dir=$(pwd)" >> "$log_file"
+    >&2 echo "$(date --rfc-3339=seconds) RUN job=$job_name dir=$(pwd)"
+    CARGO_BUILD_TARGET_DIR="$(pwd)"
+    export CARGO_BUILD_TARGET_DIR
+    cd "$src_dir"
 
->&2 echo "Starting a job=$job_name in cache_dir=$cache_dir"
+    function on_exit() {
+        local exit_code=$?
 
-echo "$(date --rfc-3339=seconds) RUN $cache_dir job=$job_name" >> "$log_file"
+        echo "$(date --rfc-3339=seconds) END job=$job_name code=$exit_code" >> "$log_file"
+        >&2 echo "$(date --rfc-3339=seconds) END job=$job_name code=$exit_code"
 
-on_exit() {
-    local exit_code=$?
+        exit $exit_code
+    }
+    trap on_exit EXIT
 
-    fs-dir-cache unlock --dir "${cache_dir}"
-    echo "$(date --rfc-3339=seconds) END $cache_dir job=$job_name code=$exit_code" >> "$log_file"
-
-    exit $exit_code
+    "$@"
 }
-trap on_exit EXIT
+export -f run_in_cache
 
 
-"$@"
+fs-dir-cache exec \
+    --key-file Cargo.lock --key-str "${CARGO_PROFILE-:dev}" --key-file flake.lock \
+    -- \
+    bash -c 'run_in_cache "$@"' _ "$@"
