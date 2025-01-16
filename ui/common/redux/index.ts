elsiribot/fedi

commit 166c978c08cda3dc11c069e42355f7a0d02200c1

fix: language setting persistence

ui/common/redux/index.ts
@@ -253,8 +253,6 @@ export function initializeCommonStore({
                 else i18n.changeLanguage(language)
             })
         } else if (language) i18n.changeLanguage(language)
-
-        unsubscribeInitialLang()
     })
 
     return () => {
@@ -265,5 +263,6 @@ export function initializeCommonStore({
         unsubscribeRecovery()
         unsubscribeStorage()
         unsubscribeMatrixPayments()
+        unsubscribeInitialLang()
     }
 }

commit 5b01e13003faa5aec6755d26702244ea2f71d314

fix: lagginess caused by regression (#6084)
fix: regression caused by 166c978

lagginess was introduced by not unsubscribing after the 1st invocation

note that it is okay to call the unsubscribe function on unmount and
it will almost always be a no-op but better hygiene to include it

ui/common/redux/index.ts
@@ -256,6 +256,8 @@ export function initializeCommonStore({
                 else i18n.changeLanguage(language)
             })
         } else if (language) i18n.changeLanguage(language)
+
+        unsubscribeInitialLang()
     })
 
     return () => {