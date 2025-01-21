804c527c9c94aaf852b1d1c0c2252ea5dbaf1a4a
Conner Ow <conner@fedi.xyz>
2025-01-21 10:22:00 -0600

    feat: include txn type in txn history details
    Addresses https://github.com/fedibtc/fedi/issues/5930
    
    To test, go to the Transactions page > click on a tranaction >
    you will see the new Type field.

--- ui/common/utils/wallet.ts
+++ ui/common/utils/wallet.ts
@@ -461,6 +461,18 @@ export const makeTxnDetailItems = (
         value: dateUtils.formatTimestamp(txn.createdAt, 'MMM dd yyyy, h:mmaaa'),
     })
 
+    let txnTypeText = t('words.unknown')
+
+    if (txn.bitcoin) txnTypeText = t('words.onchain')
+    if (txn.lightning) txnTypeText = t('words.lightning')
+    if (txn.stabilityPoolState) txnTypeText = t('phrases.stability-pool')
+    if (txn.oobState) txnTypeText = t('words.ecash')
+
+    items.push({
+        label: t('words.type'),
+        value: txnTypeText,
+    })
+
     // shows the value of ecash sent in/out of stabilitypool at today's price
     // in local currency (historical value at time of txn shows elsewhere)
     if (
@@ -648,6 +660,7 @@ export const makeStabilityTxnDetailItems = (
         label: t('words.time'),
         value: dateUtils.formatTimestamp(txn.createdAt, 'MMM dd yyyy, h:mmaaa'),
     })
+
     return items
 }
 
