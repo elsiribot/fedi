7fa10af11104dd89fe2f5cdc05d073e9a73af0f9
IroncladDev <connerow1115@gmail.com>
2025-01-24 11:30:00 -0500

    fix: don't allow sending less than 1 sat (#6044)

--- ui/common/hooks/amount.ts
+++ ui/common/hooks/amount.ts
@@ -527,7 +527,7 @@ export function useMinMaxSendAmount(
         // If balance is less than 1000 msat (rounded down to 0 sats), don't allow send at all
         if (balance < 1000)
             return {
-                minimumAmount: 0 as Sats,
+                minimumAmount: 1 as Sats,
                 maximumAmount: 0 as Sats,
             }
 
