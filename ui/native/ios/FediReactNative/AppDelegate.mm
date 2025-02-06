b4e7092466e5cea856e161bca60b6b143df6ce6c
Nadav Drewe <emailnadz@gmail.com>
2025-02-06 20:32:31 +0000

    fix: add push notification registration code to app delegate, prevent foreground Zendesk notifications firing (#6256)
    * chore: updated appdelegate
    
    * chore: update logs
    
    * chore: update zendesk version in podfile
    
    * chore: removed alert from appdelegate
    
    * chore: supress foreground notifications
    
    * chore: tweak appdelegate comments
    
    * chore: restored entitlements to development

--- ui/native/ios/FediReactNative/AppDelegate.mm
+++ ui/native/ios/FediReactNative/AppDelegate.mm
@@ -2,9 +2,16 @@
 
 // for push notifications
 #import <Firebase.h>
+#import <react-native-zendesk-messaging/ZendeskNativeModule.h>
+#import <UserNotifications/UserNotifications.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTLinkingManager.h>
 #import "RNSplashScreen.h"
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate () <UNUserNotificationCenterDelegate>
+- (void)registerForPushNotifications;
+@end
 
 @implementation AppDelegate
 
@@ -22,8 +29,12 @@
 
   // Register for remote notifications
   [application registerForRemoteNotifications];
+  [self registerForPushNotifications];
 
   [super application:application didFinishLaunchingWithOptions:launchOptions];
+
+    // Ensure UNUserNotificationCenter delegate is set
+  [UNUserNotificationCenter currentNotificationCenter].delegate = self;
 
   // for splash screen
   [RNSplashScreen show];
@@ -31,18 +42,47 @@
   return YES;
 }
 
+- (void)registerForPushNotifications {
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    center.delegate = self;
+    [center requestAuthorizationWithOptions:(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge)
+                              completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        if (granted) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [[UIApplication sharedApplication] registerForRemoteNotifications];
+            });
+            NSLog(@"Push notification permissions granted.");
+        } else {
+            NSLog(@"Push notification permissions denied.");
+        }
+    }];
+}
+
+
 // Handle APNs token registration and pass it to Firebase
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 {
   // Pass the APNs token to Firebase Messaging
   [FIRMessaging messaging].APNSToken = deviceToken;
   NSLog(@"APNs Token set in Firebase.");
+  // Pass the APNs token to Zendesk Messaging
+  [ZendeskNativeModule updatePushNotificationToken:deviceToken];
+  NSLog(@"Successfully registered APNs token with Zendesk.");
 }
 
 // Handle failed APNs registration
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
 {
   NSLog(@"Failed to register for remote notifications: %@", error);
+}
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+       willPresentNotification:(UNNotification *)notification
+         withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler
+{
+    NSLog(@"Received push notification while app is in foreground: %@", notification.request.content.userInfo);
+   // Do not show any notification banner in foreground
+    completionHandler(UNNotificationPresentationOptionNone);
 }
 
 // Provide the JS bundle URL for React Native
