1a6db6cbb9d3bc39ab6baa6306bbcd2d8a92098c
Dawid Ciężarkiewicz <dpc@dpc.pw>
2025-04-14 12:59:10 -0700

    chore: enable lnv2

--- scripts/test-bridge-current.inner.sh
+++ scripts/test-bridge-current.inner.sh
@@ -6,7 +6,6 @@ source scripts/common.sh
 export INCLUDE_STABILITY_POOL=1
 export FEDI_STABILITY_POOL_MODULE_ENABLE=1
 export FEDI_STABILITY_POOL_V2_MODULE_ENABLE=1
-export FM_DEVIMINT_DISABLE_MODULE_LNV2=1
 export USE_STABILITY_POOL_TEST_PARAMS=1
 export FEDI_STABILITY_POOL_MODULE_TEST_PARAMS=1
 export FEDI_SOCIAL_RECOVERY_MODULE_ENABLE=1
@@ -21,5 +20,5 @@ export FM_ADMIN_PASSWORD=p
 echo "## Ensuring everything built"
 cargo build --profile "${CARGO_PROFILE}" --all-targets
 echo "## Running v2 bridge tests"
-cargo nextest run -v --locked --cargo-profile "${CARGO_PROFILE}" -E 'package(fedi-ffi)' "$@"
+cargo nextest run -v --locked --cargo-profile "${CARGO_PROFILE}" -E 'package(fedi-ffi)' --nocapture "$@"
 echo "## Tests Passed"
