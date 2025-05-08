d8197c9deeb01cb5149aa19c7985a540706a70b5
tom-fedi <tom@fedi.xyz>
2025-05-08 08:24:21 +0100

    chore: hide recovery buttons in pwa (#7232)

--- ui/web/src/pages/index.tsx
+++ ui/web/src/pages/index.tsx
@@ -55,7 +55,7 @@ function WelcomePage() {
         <ContentBlock
             css={{
                 backgroundImage: `url(${welcomeBackground.src})`,
-                backgroundPosition: 'center -40px',
+                backgroundPosition: 'center center',
                 backgroundSize: 'cover',
             }}>
             <Layout.Root>
@@ -77,15 +77,15 @@ function WelcomePage() {
                         loading={loading}>
                         {t('words.continue')}
                     </Button>
-
-                    {!inviteCode && (
+                    {/* Hide for now until issues with recovery on the backend can be investigated further */}
+                    {/* {!inviteCode && (
                         <Button
                             width="full"
                             href="/onboarding/recover"
                             variant="secondary">
                             {t('phrases.recover-my-account')}
                         </Button>
-                    )}
+                    )} */}
 
                     <TextWrapper>
                         <Text
