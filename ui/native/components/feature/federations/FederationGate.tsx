c360e8dff6d2d12ef0e1b160dfafd71ba9b6d990
Conner Ow <conner@fedi.xyz>
2025-05-12 14:24:20 -0500

    fix: move permission gate logic to common
    Contributes o #6927

--- ui/native/components/feature/federations/FederationGate.tsx
+++ ui/native/components/feature/federations/FederationGate.tsx
@@ -1,13 +1,11 @@
 import { useNavigation } from '@react-navigation/native'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { ActivityIndicator } from 'react-native'
 
-import { useFederationPreview } from '@fedi/common/hooks/federation'
-import { selectFederations } from '@fedi/common/redux'
+import { useFederationMembership } from '@fedi/common/hooks/federation'
 
 import { fedimint } from '../../../bridge'
-import { useAppSelector } from '../../../state/hooks'
 import CustomOverlay from '../../ui/CustomOverlay'
 import FederationPreview from '../onboarding/FederationPreview'
 
@@ -23,29 +21,16 @@ export default function FederationGate({
     fallbackContent,
 }: Props) {
     const [isJoining, setIsJoining] = useState(true)
-    const federations = useAppSelector(selectFederations)
     const navigation = useNavigation()
     const { t } = useTranslation()
-    const { isFetchingPreview, federationPreview, handleCode, handleJoin } =
-        useFederationPreview(t, fedimint, '')
+    const { isFetchingPreview, federationPreview, isMember, handleJoin } =
+        useFederationMembership(t, fedimint, inviteCode)
 
     const handleBack = () => {
         if (navigation.canGoBack()) navigation.goBack()
     }
 
-    const foundFederation = federations.find(f => {
-        if (f.init_state === 'ready') return f.inviteCode === inviteCode
-
-        return false
-    })
-
-    useEffect(() => {
-        if (foundFederation) return
-
-        handleCode(inviteCode)
-    }, [foundFederation, inviteCode, handleCode])
-
-    if (foundFederation) return children
+    if (isMember) return children
 
     return (
         <>
