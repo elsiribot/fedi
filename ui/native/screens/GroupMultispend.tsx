61d65219e38fa78d022108bee9b885c2c833198a
IroncladDev <connerow1115@gmail.com>
2025-06-04 02:07:15 -0500

    feat: show multispend failed state (#7609)
    Addresses #7594
    
    Shows a failed state on the GroupMultispend screen if the group
    multispend status is not in an activeInvitaion or finalized state
    
    Co-authored-by: Conner Ow <conner@fedi.xyz>

--- ui/native/screens/GroupMultispend.tsx
+++ ui/native/screens/GroupMultispend.tsx
@@ -1,4 +1,5 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { Text, Button } from '@rneui/themed'
 import { useTranslation } from 'react-i18next'
 
 import { useObserveMultispendAccountInfo } from '@fedi/common/hooks/matrix'
@@ -10,7 +11,9 @@ import MultispendWalletHeader from '../components/feature/multispend/MultispendW
 import MultispendFinalized from '../components/feature/multispend/finalized/MultispendFinalized'
 import MultispendActiveInvitation from '../components/feature/multispend/invitation/MultispendActiveInvitation'
 import Flex from '../components/ui/Flex'
+import { SafeAreaContainer } from '../components/ui/SafeArea'
 import { useAppSelector } from '../state/hooks'
+import { reset } from '../state/navigation'
 import { RootStackParamList } from '../types/navigation'
 
 export type Props = NativeStackScreenProps<
@@ -18,7 +21,7 @@ export type Props = NativeStackScreenProps<
     'GroupMultispend'
 >
 
-const GroupMultispend: React.FC<Props> = ({ route }: Props) => {
+const GroupMultispend: React.FC<Props> = ({ route, navigation }: Props) => {
     const { roomId } = route.params
 
     useObserveMultispendAccountInfo(roomId)
@@ -30,7 +33,29 @@ const GroupMultispend: React.FC<Props> = ({ route }: Props) => {
     const { t } = useTranslation()
     const { shouldShowHeader } = useMultispendDisplayUtils(t, roomId)
 
-    if (!multispendStatus) return null
+    if (!multispendStatus)
+        return (
+            <SafeAreaContainer edges="all">
+                <Flex grow center gap="md">
+                    <Text h2>
+                        {t('feature.multispend.multispend-unavailable')}
+                    </Text>
+                    <Text caption center>
+                        {t(
+                            'feature.multispend.multispend-unavailable-description',
+                        )}
+                    </Text>
+                </Flex>
+                <Button
+                    title={t('phrases.go-back')}
+                    onPress={() => {
+                        navigation.dispatch(
+                            reset('ChatRoomConversation', { roomId }),
+                        )
+                    }}
+                />
+            </SafeAreaContainer>
+        )
 
     return (
         <MultispendFederationGate roomId={roomId}>
