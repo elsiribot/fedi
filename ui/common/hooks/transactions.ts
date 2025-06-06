ec2eab88f9a675b110f1651d7b3178d1acdcfea7
IroncladDev <connerow1115@gmail.com>
2025-06-06 17:54:17 -0500

    fix: show plus or minus sign on multispend txn amounts except rejections (#7674)
    Finalizes #7359
    
    - Adds a minus or plus sign to multispend transactions
    - Does not show a minus or plus sign for failed or rejected transactions
    
    Co-authored-by: Conner Ow <conner@fedi.xyz>

--- ui/common/hooks/transactions.ts
+++ ui/common/hooks/transactions.ts
@@ -287,16 +287,24 @@ export function useMultispendTxnDisplayUtils(t: TFunction, roomId: RpcRoomId) {
             if (txn.state === 'deposit') {
                 const fiatAmount = txn.event.depositNotification
                     .fiatAmount as UsdCents
-                return `${convertCentsToFormattedFiat(fiatAmount, 'none')}${includeCurrency ? ` ${preferredCurrency || SupportedCurrency.USD}` : ''}`
+                return `+${convertCentsToFormattedFiat(fiatAmount, 'none')}${includeCurrency ? ` ${preferredCurrency || SupportedCurrency.USD}` : ''}`
             }
             if (txn.state === 'withdrawal') {
+                const isRejected = multispendStatus
+                    ? isWithdrawalRequestRejected(txn, multispendStatus)
+                    : true
+                const hasFailed =
+                    txn.event.withdrawalRequest.txSubmissionStatus !==
+                        'unknown' &&
+                    'rejected' in txn.event.withdrawalRequest.txSubmissionStatus
+
                 const fiatAmount = txn.event.withdrawalRequest.request
                     .transfer_amount as UsdCents
-                return `${convertCentsToFormattedFiat(fiatAmount, 'none')}${includeCurrency ? ` ${preferredCurrency || SupportedCurrency.USD}` : ''}`
+                return `${isRejected || hasFailed ? '' : '-'}${convertCentsToFormattedFiat(fiatAmount, 'none')}${includeCurrency ? ` ${preferredCurrency || SupportedCurrency.USD}` : ''}`
             }
             return '-'
         },
-        [convertCentsToFormattedFiat, preferredCurrency],
+        [convertCentsToFormattedFiat, preferredCurrency, multispendStatus],
     )
 
     const makeMultispendTxnCurrencyText = useCallback(() => {
