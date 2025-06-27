fd5ee350f3c6952a680525d266d4b4ad7d75be7e
Conner Ow <conner@fedi.xyz>
2025-06-27 16:41:41 -0500

    feat: fullscreen avatar url from chat settings
    Addresses #4563
    
    Shows a fullscreen avatar viewer if you click on the user's avatar from
    settings

--- ui/native/components/feature/chat/ChatSettingsAvatar.tsx
+++ ui/native/components/feature/chat/ChatSettingsAvatar.tsx
@@ -1,6 +1,7 @@
+import { useNavigation } from '@react-navigation/native'
 import { Text, Theme, useTheme } from '@rneui/themed'
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { Pressable, StyleSheet } from 'react-native'
 
 import { MatrixRoom } from '@fedi/common/types'
 
@@ -15,19 +16,28 @@ interface Props {
 
 export const ChatSettingsAvatar: React.FC<Props> = ({ room }) => {
     const { theme } = useTheme()
+    const navigation = useNavigation()
 
     const style = styles(theme)
+
+    const handleAvatarPress = () => {
+        if (room && room.directUserId && room.avatarUrl) {
+            navigation.navigate('ChatImageViewer', { uri: room.avatarUrl })
+        }
+    }
 
     if (!room) return <HoloLoader />
 
     return (
         <Flex center style={style.container}>
-            <ChatAvatar
-                room={room}
-                size={AvatarSize.lg}
-                containerStyle={style.avatar}
-                maxFontSizeMultiplier={1}
-            />
+            <Pressable onPress={handleAvatarPress}>
+                <ChatAvatar
+                    room={room}
+                    size={AvatarSize.lg}
+                    containerStyle={style.avatar}
+                    maxFontSizeMultiplier={1}
+                />
+            </Pressable>
             <Text h2 style={style.roomName} numberOfLines={1}>
                 {room?.name || ''}
             </Text>
