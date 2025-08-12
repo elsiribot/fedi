36a8b23808bb4acaf8c796983e482c36675acda6
Conner Ow <conner@fedi.xyz>
2025-08-12 14:46:15 -0500

    chore: tests for survey ui

---
+++ ui/native/tests/components/feature/home/SurveyOverlay.test.tsx
@@ -0,0 +1,22 @@
+import { screen } from '@testing-library/react-native'
+
+import i18n from '@fedi/native/localization/i18n'
+
+import SurveyOverlay from '../../../../components/feature/home/SurveyOverlay'
+import { renderWithProviders } from '../../../utils/render'
+
+describe('SurveyModal', () => {
+    it('should render with the correct title, description, and button', async () => {
+        renderWithProviders(<SurveyOverlay open onOpenChange={() => {}} />)
+
+        const title = screen.getByText(i18n.t('feature.support.survey-title'))
+        const description = screen.getByText(
+            i18n.t('feature.support.survey-description'),
+        )
+        const button = screen.getByText(i18n.t('feature.support.give-feedback'))
+
+        expect(title).toBeOnTheScreen()
+        expect(description).toBeOnTheScreen()
+        expect(button).toBeOnTheScreen()
+    })
+})
