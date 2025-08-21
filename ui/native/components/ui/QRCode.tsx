11ecf60e0a8a33695905a450b17450f45ff8f8e3
Conner Ow <conner@fedi.xyz>
2025-08-21 15:28:44 -0500

    feat: allow users to download QR codes
    Addresses #4950
    
    Allows users to download any QR codes across the app except for partial
    ecash QRs by long-pressing on them
    
    Opens up the `react-native-share` dialog with options to share, copy,
    and download the image

--- ui/native/components/ui/QRCode.tsx
+++ ui/native/components/ui/QRCode.tsx
@@ -1,20 +1,98 @@
 import { Theme, useTheme } from '@rneui/themed'
-import { useMemo } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { useCallback, useMemo, useRef } from 'react'
+import { useTranslation } from 'react-i18next'
+import { Animated, Pressable, StyleSheet } from 'react-native'
+import { exists } from 'react-native-fs'
+import { PermissionStatus, RESULTS } from 'react-native-permissions'
+import { Easing } from 'react-native-reanimated'
+import Share from 'react-native-share'
 import { SvgXml } from 'react-native-svg'
+import ViewShot from 'react-native-view-shot'
 
+import { useToast } from '@fedi/common/hooks/toast'
 import { renderStyledQrSvg } from '@fedi/common/utils/qrcode'
+
+import { useDownloadPermission } from '../../utils/hooks'
 
 interface Props {
     value: string
     size: number
     logoOverrideUrl?: string
+    disableSave?: boolean
 }
 
-const QRCode: React.FC<Props> = ({ value, size, logoOverrideUrl }) => {
+const QRCode: React.FC<Props> = ({
+    value,
+    size,
+    logoOverrideUrl,
+    disableSave,
+}) => {
+    const animatedPadding = useRef(new Animated.Value(0)).current
+    const viewShotRef = useRef<ViewShot>(null)
+    const toast = useToast()
+
     const { theme } = useTheme()
+    const { t } = useTranslation()
+    const { requestDownloadPermission, downloadPermission } =
+        useDownloadPermission()
 
     const style = styles(theme)
+
+    const handleLongPress = useCallback(async () => {
+        if (disableSave) return
+
+        Animated.timing(animatedPadding, {
+            toValue: 16,
+            duration: 150,
+            delay: 0,
+            useNativeDriver: false,
+            easing: Easing.out(Easing.ease),
+        }).start()
+
+        // wait for the animation to complete before attempting to screenshot
+        await new Promise(resolve => setTimeout(() => resolve(true), 150))
+
+        try {
+            let permissionStatus: PermissionStatus | undefined =
+                downloadPermission
+
+            if (downloadPermission !== RESULTS.GRANTED)
+                permissionStatus = await requestDownloadPermission()
+
+            if (permissionStatus === RESULTS.GRANTED) {
+                const qrUri = await viewShotRef.current?.capture?.()
+
+                if (!qrUri || !(await exists(qrUri))) return
+
+                try {
+                    await Share.open({
+                        url: qrUri,
+                    })
+                } catch {
+                    /* no-op, user cancelled */
+                }
+            } else {
+                throw new Error(t('errors.please-grant-permission'))
+            }
+        } catch (e) {
+            toast.error(t, e)
+        } finally {
+            Animated.timing(animatedPadding, {
+                toValue: 0,
+                duration: 150,
+                delay: 0,
+                useNativeDriver: false,
+                easing: Easing.out(Easing.ease),
+            }).start()
+        }
+    }, [
+        animatedPadding,
+        downloadPermission,
+        requestDownloadPermission,
+        t,
+        toast,
+        disableSave,
+    ])
 
     const xml = useMemo(
         () =>
@@ -27,9 +105,19 @@ const QRCode: React.FC<Props> = ({ value, size, logoOverrideUrl }) => {
     )
 
     return (
-        <View style={[style.container, { width: size, height: size }]}>
-            <SvgXml xml={xml} />
-        </View>
+        <Pressable onLongPress={handleLongPress} delayLongPress={300}>
+            <ViewShot
+                ref={viewShotRef}
+                options={{ format: 'png', fileName: 'fedi-qr-code.png' }}>
+                <Animated.View
+                    style={[
+                        style.container,
+                        { width: size, height: size, padding: animatedPadding },
+                    ]}>
+                    <SvgXml xml={xml} />
+                </Animated.View>
+            </ViewShot>
+        </Pressable>
     )
 }
 
