03ab53915418499330d523213763a5e375e1fb00
Conner Ow <conner@fedi.xyz>
2025-09-08 13:57:27 -0500

    chore: integration tests for `common` `useMakeOnchainAddress` hook
    Contributes to #8552
    
    Adds an integration test suite for the `useMakeOnchainAddress` hook

--- ui/common/tests/integration/hooks/receive.test.ts
+++ ui/common/tests/integration/hooks/receive.test.ts
@@ -4,7 +4,10 @@
  */
 import { act, waitFor } from '@testing-library/react'
 
-import { useMakeLightningRequest } from '../../../hooks/receive'
+import {
+    useMakeLightningRequest,
+    useMakeOnchainAddress,
+} from '../../../hooks/receive'
 import { selectLastUsedFederationId } from '../../../redux'
 import { Sats } from '../../../types'
 import { createIntegrationTestBuilder } from '../../../utils/test-utils/remote-bridge-setup'
@@ -109,6 +112,140 @@ describe('receiving payments', () => {
 
             await waitFor(() => {
                 expect(onInvoicePaid).toHaveBeenCalled()
+            })
+        }, 30000)
+    })
+
+    describe('useMakeOnchainAddress', () => {
+        it('should create an onchain address with makeOnchainAddress()', async () => {
+            await builder.withFederationJoined()
+
+            const {
+                store,
+                remoteBridge: { fedimint },
+                renderHookWithBridge,
+            } = context
+
+            const federationId = selectLastUsedFederationId(store.getState())
+            const { result } = renderHookWithBridge(() =>
+                useMakeOnchainAddress({
+                    fedimint,
+                    federationId,
+                }),
+            )
+
+            // Make the onchain address
+            await act(() => result.current.makeOnchainAddress())
+
+            // Wait for the address
+            await waitFor(() => {
+                expect(result.current.address).toBeTruthy()
+            })
+        }, 30000)
+
+        it('should save notes with onSaveNotes', async () => {
+            await builder.withFederationJoined()
+
+            const {
+                store,
+                remoteBridge: { fedimint },
+                renderHookWithBridge,
+            } = context
+
+            const federationId = selectLastUsedFederationId(store.getState())
+            const { result } = renderHookWithBridge(() =>
+                useMakeOnchainAddress({ fedimint, federationId }),
+            )
+
+            // Make an onchain address
+            await act(() => result.current.makeOnchainAddress())
+
+            // Wait for the address and its respective transaction ID
+            await waitFor(() => {
+                expect(result.current.address).toBeDefined()
+                expect(result.current.transaction).toBeDefined()
+            })
+
+            // Save notes
+            await act(() => result.current.onSaveNotes('test notes'))
+
+            // Fetch the transaction by its ID and ensure that notes match
+            await waitFor(async () => {
+                expect(result.current.transaction).toBeDefined()
+
+                const transaction = await fedimint.getTransaction(
+                    federationId || '',
+                    result.current.transaction?.id || '',
+                )
+
+                expect(transaction.txnNotes).toBe('test notes')
+            })
+        }, 30000)
+
+        it('should call the `onMakeAddressError` callback if supplied, if `makeOnchainAddress` errors', async () => {
+            await builder.withFederationJoined()
+
+            const {
+                remoteBridge: { fedimint },
+                renderHookWithBridge,
+            } = context
+
+            const onMakeAddressError = jest.fn()
+
+            const { result } = renderHookWithBridge(() =>
+                useMakeOnchainAddress({
+                    fedimint,
+                    federationId: 'invalid federation id',
+                    onMakeAddressError,
+                }),
+            )
+
+            // Make the onchain address
+            await act(() => result.current.makeOnchainAddress())
+
+            await waitFor(() => {
+                expect(onMakeAddressError).toHaveBeenCalled()
+            })
+        }, 30000)
+
+        it('should call the `onSaveNotesError` callback if supplied, if `onSaveNotes` errors', async () => {
+            await builder.withFederationJoined()
+
+            const {
+                store,
+                remoteBridge: { fedimint },
+                renderHookWithBridge,
+            } = context
+
+            const onSaveNotesError = jest.fn()
+
+            const federationId = selectLastUsedFederationId(store.getState())
+            const { result } = renderHookWithBridge(() =>
+                useMakeOnchainAddress({
+                    fedimint,
+                    federationId,
+                    onSaveNotesError,
+                }),
+            )
+
+            // Make an onchain address
+            await act(() => result.current.makeOnchainAddress())
+
+            // Wait for the address and its respective transaction ID
+            await waitFor(() => {
+                expect(result.current.address).toBeDefined()
+                expect(result.current.transaction).toBeDefined()
+            })
+
+            // Save notes
+            await act(() =>
+                result.current.onSaveNotes({
+                    this: 'is a test',
+                } as unknown as string),
+            )
+
+            await waitFor(async () => {
+                expect(onSaveNotesError).toHaveBeenCalled()
             })
         }, 30000)
     })
