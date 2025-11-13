b4b38cf2ae7baca0965daf6bf69297773581bc37
oz <otech47@gmail.com>
2025-11-13 10:38:47 +0100

    chore: bump injections version to 2 (#9417)
    * chore: bump injections version to 2
    
    - since community RPCs have breaking changes
    
    * chore: allow devs to use nightly miniapps
    
    - specifically community generator tool without running one locally

--- ui/native/screens/PublicCommunities.tsx
+++ ui/native/screens/PublicCommunities.tsx
@@ -6,7 +6,7 @@ import { ScrollView, StyleSheet, View } from 'react-native'
 
 import { useLatestPublicCommunities } from '@fedi/common/hooks/federation'
 import { selectCommunityIds } from '@fedi/common/redux'
-import { isNightly } from '@fedi/common/utils/environment'
+import { isDev, isNightly } from '@fedi/common/utils/environment'
 import { Images } from '@fedi/native/assets/images'
 
 import { FederationLogo } from '../components/feature/federations/FederationLogo'
@@ -232,9 +232,10 @@ const PublicCommunities: React.FC<Props> = ({ navigation }) => {
                         title={t('phrases.create-my-community')}
                         onPress={() =>
                             navigation.navigate('FediModBrowser', {
-                                url: isNightly()
-                                    ? COMMUNITY_TOOL_URL_STAGING
-                                    : COMMUNITY_TOOL_URL_PROD,
+                                url:
+                                    isNightly() || isDev()
+                                        ? COMMUNITY_TOOL_URL_STAGING
+                                        : COMMUNITY_TOOL_URL_PROD,
                             })
                         }
                     />
