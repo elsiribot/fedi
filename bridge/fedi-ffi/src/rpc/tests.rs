5636dae07eb924ec851ca5158a263571ad3f3a96
maan2003 <manmeetmann2003@gmail.com>
2025-12-24 09:18:23 +0000

    fix: flaky onchain tests
    I have seen this in WaitingForConfirmation
    
    in general this can be in any state but failed and we assertions below
    anyways

--- bridge/fedi-ffi/src/rpc/tests.rs
+++ bridge/fedi-ffi/src/rpc/tests.rs
@@ -679,10 +679,7 @@ async fn test_on_chain_with_fedi_fees(
         listTransactions(federation.clone(), None, None).await?[0],
         Ok(RpcTransactionListEntry {
             transaction: RpcTransaction {
-                kind: RpcTransactionKind::OnchainDeposit {
-                    state: Some(RpcOnchainDepositState::WaitingForTransaction),
-                    ..
-                },
+                kind: RpcTransactionKind::OnchainDeposit { state: Some(_), .. },
                 ..
             },
             ..
@@ -775,10 +772,7 @@ async fn test_on_chain_with_fedi_fees_with_restart(
         listTransactions(federation.clone(), None, None).await?[0],
         Ok(RpcTransactionListEntry {
             transaction: RpcTransaction {
-                kind: RpcTransactionKind::OnchainDeposit {
-                    state: Some(RpcOnchainDepositState::WaitingForTransaction),
-                    ..
-                },
+                kind: RpcTransactionKind::OnchainDeposit { state: Some(_), .. },
                 ..
             },
             ..
