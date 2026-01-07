b2324f295382f7e29d12b5f084e14c323372556b
Conner Ow <conner@ironclad.sh>
2026-01-07 07:52:50 -0600

    feat: update for web

--- ui/web/src/hooks/browser.ts
+++ ui/web/src/hooks/browser.ts
@@ -7,6 +7,7 @@ import {
     joinCommunity,
     refreshCommunities,
     selectCommunities,
+    selectMatrixRooms,
     selectNostrNpub,
     selectPaymentFederation,
     setInvoiceToPay,
@@ -62,6 +63,7 @@ export function useIFrameListener(
     const nostrPublic = useAppSelector(selectNostrNpub)
     const paymentFederation = useAppSelector(selectPaymentFederation)
     const communities = useAppSelector(selectCommunities)
+    const chats = useAppSelector(selectMatrixRooms)
     const [overlayId, setOverlayId] = useState<InjectionMessageType | null>(
         null,
     )
@@ -321,6 +323,21 @@ export function useIFrameListener(
                     }
                     break
                 }
+                case InjectionMessageType.fedi_previewMatrixRoom: {
+                    const room = chats.find(
+                        chat => chat.isPublic && chat.id === payload,
+                    )
+                    sendSuccess(
+                        event,
+                        room
+                            ? {
+                                  name: room.name,
+                                  avatarUrl: room.avatarUrl,
+                              }
+                            : null,
+                    )
+                    break
+                }
             }
         }
 
@@ -336,6 +353,7 @@ export function useIFrameListener(
         replace,
         sendSuccess,
         sendError,
+        chats,
     ])
 
     return { sendSuccess, sendError, overlayId, resetOverlay }
