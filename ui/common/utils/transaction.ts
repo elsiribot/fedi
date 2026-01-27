62f61b3cedce488407133ed0af8f4f3da1fcd919
IroncladDev <conner@ironclad.sh>
2026-01-27 11:05:00 -0600

    chore: add unit test coverage for makeMultispendTxnStatusText

--- ui/common/utils/transaction.ts
+++ ui/common/utils/transaction.ts
@@ -1005,6 +1005,7 @@ export const makeMultispendTxnStatusText = (
     )
         return t('words.unknown')
 
+    // TODO+TEST: Perhaps using `is Type` assertion functions would be better than `in txn.event`
     if ('depositNotification' in txn.event)
         return csvExport ? t('words.complete') : t('words.deposit')
     if ('withdrawalRequest' in txn.event) {
