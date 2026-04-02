310c2837aa201454ee0a7ce960e14dbf458cf7eb
IroncladDev <conner@ironclad.sh>
2026-04-02 09:55:20 -0500

    chore: (native) remove layout shift and add help tooltips to payment switcher tabs
    - Adds an auto-sizing `HelpTooltip` component to display in the switcher
      tabs
    - Instead of hiding switcher that federations don't support, tabs now get
      disabled and display a help tooltip explaining why. This also reduces
      unnecessary layout shifts. **This is important for #10804**, because,
      although unlikely, some federations don't support onchain deposits.
      Hiding the "onchain" tab like we did previously would leave the user
      in a stuck state.
    
    ## PWA Parity
    
    Will be implemented in a separate PR
    
    ## Automated Testing
    
    Will be implemented in a separate PR

---
+++ ui/native/components/ui/HelpTooltip.tsx
@@ -0,0 +1,75 @@
+import { Theme, Tooltip, useTheme } from '@rneui/themed'
+import { useState } from 'react'
+import { ScaledSize, StyleSheet, useWindowDimensions, View } from 'react-native'
+
+import { PressableIcon } from './PressableIcon'
+
+/**
+ * Displays a tooltip with a "Help" icon that is used to display information on press.
+ * Includes a backdrop.
+ * Automatically sizes itself to fit the tooltip content.
+ */
+export default function HelpTooltip({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    const [open, setOpen] = useState(false)
+    const [tooltipWidth, setTooltipWidth] = useState(0)
+    const [tooltipHeight, setTooltipHeight] = useState(0)
+    const { theme } = useTheme()
+    const dimensions = useWindowDimensions()
+
+    const style = styles(theme, dimensions)
+
+    return (
+        <>
+            <Tooltip
+                visible={open}
+                onClose={() => setOpen(false)}
+                onOpen={() => setOpen(true)}
+                closeOnlyOnBackdropPress
+                withOverlay
+                overlayColor={theme.colors.overlay}
+                backgroundColor={theme.colors.blue100}
+                width={tooltipWidth}
+                height={tooltipHeight}
+                containerStyle={{ padding: 0 }}
+                popover={<View style={style.contentWrapper}>{children}</View>}>
+                <PressableIcon
+                    svgName="Help"
+                    onPress={e => {
+                        e.stopPropagation()
+                        setOpen(true)
+                    }}
+                    svgProps={{
+                        color: theme.colors.night,
+                        size: 20,
+                    }}
+                />
+            </Tooltip>
+            <View
+                style={style.invisibleContent}
+                onLayout={e => {
+                    setTooltipWidth(e.nativeEvent.layout.width)
+                    setTooltipHeight(e.nativeEvent.layout.height)
+                }}>
+                <View style={style.contentWrapper}>{children}</View>
+            </View>
+        </>
+    )
+}
+
+const styles = (theme: Theme, dimensions: ScaledSize) =>
+    StyleSheet.create({
+        invisibleContent: {
+            opacity: 0,
+            position: 'absolute',
+            top: dimensions.height,
+        },
+        contentWrapper: {
+            maxWidth: dimensions.width * 0.75,
+            paddingHorizontal: theme.spacing.md,
+            paddingVertical: theme.spacing.sm,
+        },
+    })
